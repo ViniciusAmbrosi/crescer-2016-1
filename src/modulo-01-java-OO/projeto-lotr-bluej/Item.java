c1bbbde7c5793332b7873dc248c6779f20778fdb
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-04-07 17:30:27 -0300

    feat(Item,ItemTest): adiciona e define classe item e classe de teste.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Item.java
@@ -0,0 +1,16 @@
+
+public class Item{    
+    private int qtd;
+    private String descricao;
+    
+    public Item (int qtd, String descricao){
+        this.qtd = qtd;
+        this.descricao = descricao;
+    }
+    
+    public int getQtd(){return this.qtd;}
+    
+    public String getDescricao(){return this.descricao;}
+    
+    
+}
---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ItemTest.java
@@ -0,0 +1,15 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ItemTest{
+    @Test
+    public void cria1ItemEspada(){
+        Item item = new Item(1,"espada");
+        assertEquals(1, item.getQtd());
+        assertTrue(item.getDescricao().equals("espada"));
+    }
+}
