8ff6f624237d5d74e4a44e2196a9d9e9c10f2b40
Vinicius A <Vinicius A>
2016-04-06 21:24:19 -0300

    feat(DataTerceiraEra): criação e definição DataTerceiraEra.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEra.java
@@ -0,0 +1,22 @@
+public class DataTerceiraEra{
+    
+    private int dia, mes, ano;
+    
+    public DataTerceiraEra(int dia, int mes, int ano){ //Validação não definida no exercício, logo(?)
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+    
+    public int getDia(){return this.dia;}
+    
+    public int getMes(){return this.mes;}
+    
+    public int getAno(){return this.ano;}
+    
+    public boolean ehBissexto(){ //é bissexto aquele que é múltiplo de 400 ou é multiplo de 4 mas nao de 100
+        boolean multQuatrocentos = this.ano % 400 == 0; //É multiplo de Quatrocentos?
+        boolean multQuatroNaoMultCem = this.ano % 4 == 0 && this.ano % 100 != 0; //É múltiplo de 4 sem ser de 100?
+        return multQuatrocentos ? true : multQuatroNaoMultCem; //Se é multiplo de quatrocentos, true, se não, é multiplo de 4 sem ser de 100?
+    }
+}
---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEraTest.java
@@ -0,0 +1,15 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DataTerceiraEraTest
+{   
+    @Test
+    public void criaDataTerceiraEra(){
+        DataTerceiraEra dte = new DataTerceiraEra();
+    }
+        
+}
