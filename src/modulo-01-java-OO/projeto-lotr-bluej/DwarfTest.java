c5641f1c727553e4eb2417b3c32315b662976427
Vinicius A <Vinicius A>
2016-04-07 20:00:32 -0300

    feat(DwarfTest): adiciona testes para novos metodos implementados em dwarf.

--- src/modulo-01-java-OO/projeto-lotr-bluej/DwarfTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DwarfTest.java
@@ -8,7 +8,7 @@ public class DwarfTest
     @Test
     public void testeConstrutorNome(){
         Dwarf dwarf = new Dwarf("nome");
-        assertEquals("nome", dwarf.getNome());
+        assertEquals("nome", dwarf.getName());
     }
     
     @Test
@@ -45,4 +45,46 @@ public class DwarfTest
         dwarf.perdeVida();
         assertEquals(100, dwarf.getVida());
     }
+    
+    @Test
+    public void anaoTemInventarioDataNascDefault(){
+        Dwarf dwarf = new Dwarf("nome");
+        assertTrue(dwarf.getInventario() != null);
+        assertEquals(1, dwarf.getDataNasc().getDia());
+        assertEquals(1, dwarf.getDataNasc().getMes());
+        assertEquals(1, dwarf.getDataNasc().getAno());
+    }
+    
+    @Test
+    public void anaoNovoConstrutorDataNasc222(){
+        DataTerceiraEra dte = new DataTerceiraEra(2,2,2);
+        Dwarf dwarf = new Dwarf("nome",dte);
+        assertEquals("nome", dwarf.getName());
+        assertEquals(110, dwarf.getVida());
+        assertEquals(Status.VIVO, dwarf.getStatus());
+        assertTrue(dwarf.getInventario() != null);
+        assertEquals(2, dwarf.getDataNasc().getDia());
+        assertEquals(2, dwarf.getDataNasc().getMes());
+        assertEquals(2, dwarf.getDataNasc().getAno());
+    }
+    
+    @Test
+    public void anaoAdicionaItemInventario(){
+        Dwarf dwarf = new Dwarf("nome");
+        Item item = new Item(1,"espada");
+        dwarf.adicionarItem(item);
+        assertTrue(dwarf.getInventario().getItens().get(0) != null);
+    }
+    
+    @Test
+    public void anaoRemoveItemEspada(){
+        Dwarf dwarf = new Dwarf("nome");
+        Item espada = new Item(1,"espada");
+        Item arco = new Item(1,"arco");
+        dwarf.adicionarItem(espada);
+        dwarf.adicionarItem(arco); 
+        dwarf.perderItem(espada);
+        assertTrue(dwarf.getInventario().getItens().get(0) == arco);
+    }
+    
 }
