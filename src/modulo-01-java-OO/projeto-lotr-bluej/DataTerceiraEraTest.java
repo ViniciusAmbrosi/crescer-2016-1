b701ea5e56915247ab1834208260238f58740c6b
Vinicius A <Vinicius A>
2016-04-06 21:32:21 -0300

    feat(DataTerceiraEraTest): cria e define DataTerceiraEraTest.

--- src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEraTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEraTest.java
@@ -7,9 +7,41 @@ import org.junit.Test;
 
 public class DataTerceiraEraTest
 {   
+    public void criaDataTerceiraEraAno(){
+        DataTerceiraEra dte = new DataTerceiraEra(15,12,200);
+        assertEquals(15, dte.getDia());
+        assertEquals(12, dte.getMes());
+        assertEquals(200, dte.getAno());
+    }
+    
     @Test
-    public void criaDataTerceiraEra(){
-        DataTerceiraEra dte = new DataTerceiraEra();
+    public void criaDataTerceiraEraAno200(){
+        DataTerceiraEra dte = new DataTerceiraEra(15,12,200);
+        assertEquals(false, dte.ehBissexto());
+    }
+    
+    @Test
+    public void criaDataTerceiraEraAno400(){
+        DataTerceiraEra dte = new DataTerceiraEra(15,12,400);
+        assertEquals(true, dte.ehBissexto());
+    }
+    
+    @Test
+    public void criaDataTerceiraEraAno9000(){
+        DataTerceiraEra dte = new DataTerceiraEra(15,12,9000);
+        assertEquals(false, dte.ehBissexto());
+    }
+    
+    @Test
+    public void criaDataTerceiraEraAno2000(){
+        DataTerceiraEra dte = new DataTerceiraEra(15,12,2000);
+        assertEquals(true, dte.ehBissexto());
+    }
+    
+    @Test
+    public void criaDataTerceiraEraAno1500(){
+        DataTerceiraEra dte = new DataTerceiraEra(15,12,1500);
+        assertEquals(false, dte.ehBissexto());
     }
         
 }
