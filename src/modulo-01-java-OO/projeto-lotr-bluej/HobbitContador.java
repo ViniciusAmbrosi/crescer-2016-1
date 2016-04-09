4ce154d8c47712af73476bfc5f4d9ec287e79778
Vinicius A <Vinicius A>
2016-04-09 11:11:23 -0300

    refactory(HobbitContador): simplifica/otimiza o método calcularDiferenca para tratar somente de pares.

--- src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
@@ -3,22 +3,16 @@ import java.util.*;
 public class HobbitContador{
     //arrayDePares.get(0).get(0)
     public int calcularDiferenca(ArrayList<ArrayList<Integer>> arrayDePares){
-        ArrayList<Integer> produtoPares = new ArrayList<>(); 
-        ArrayList<Integer> mmcPares = new ArrayList<>();
-        int produtoParesAux = 1; 
+        int produtoPares= 1; 
+        int mmcPares= 0;
         int produtosMenosMmc = 0;
         for(int i = 0; i < arrayDePares.size(); i++){
-            for(int j = 0; j < arrayDePares.get(i).size(); j++){
-                produtoParesAux *= arrayDePares.get(i).get(j); //guarda produto dos pares
-                mmcPares.add(arrayDePares.get(i).get(j)); //guarda mmcDosPares
-            }
-            mmcPares.set(i, mmc(mmcPares.get(i), mmcPares.get(i+1)));
-            mmcPares.remove(i + 1); //remove segundo valor do par
-            produtoPares.add(produtoParesAux);   
-            produtosMenosMmc += produtoPares.get(i) - mmcPares.get(i);
-            produtoParesAux = 1;
-        } 
-        //return soma da dif entre o produto e o minimo múltiplo comum de cada par
+            produtoPares = arrayDePares.get(i).get(0) * 
+                           arrayDePares.get(i).get(1); //guarda produto dos pares
+            mmcPares = mmc(arrayDePares.get(i).get(0), 
+                           arrayDePares.get(i).get(1));
+            produtosMenosMmc += produtoPares - mmcPares;
+        }  
         return produtosMenosMmc;
     }
     
@@ -27,8 +21,7 @@ public class HobbitContador{
         while(a % b != 0) 
             a += aux;
         return a;
-    }
-    
+    }    
     //método de diferença simplificado para pares utilizando forEach
     public int calcularDiferencaComForEach(ArrayList<ArrayList<Integer>> arrayDePares){
         int produtoPares= 1; 
