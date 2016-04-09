b5613a97057951e61c5ed9b7f2bf3f210cdaf031
Vinicius A <Vinicius A>
2016-04-09 11:13:29 -0300

    refactory(HobbitContador): remove misleading comments.

--- src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
@@ -1,14 +1,13 @@
 import java.util.*;
 
 public class HobbitContador{
-    //arrayDePares.get(0).get(0)
     public int calcularDiferenca(ArrayList<ArrayList<Integer>> arrayDePares){
         int produtoPares= 1; 
         int mmcPares= 0;
         int produtosMenosMmc = 0;
         for(int i = 0; i < arrayDePares.size(); i++){
             produtoPares = arrayDePares.get(i).get(0) * 
-                           arrayDePares.get(i).get(1); //guarda produto dos pares
+                           arrayDePares.get(i).get(1);
             mmcPares = mmc(arrayDePares.get(i).get(0), 
                            arrayDePares.get(i).get(1));
             produtosMenosMmc += produtoPares - mmcPares;
