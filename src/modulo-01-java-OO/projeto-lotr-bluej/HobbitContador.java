6ff40d126d88fa57dfbcdc5ee1cbf51db1046c5d
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-04-11 14:10:07 -0300

    fix(HobbitContador): adiciona alguns comentarios de correçao nao antes escritos.

--- src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
@@ -30,7 +30,7 @@ public class HobbitContador{
     
     public int obterMaiorMultiploDeTresAte(int numero){ 
         int maior = 0; //retorna maior, logo nao é necessario ArrayList
-        for (int i = 3; i <= numero; i += 3){ //começar com i em 3, já que se i < 3 nao vai ser multiplo do mesmo. 
+        for (int i = 3; i <= numero; i += 3){ //começar com i em 3, já que se i < 3 nao vai ser multiplo do mesmo. Variavel utilizada como parametro invalida.
             if (i % 3 == 0 && i > maior){ //nao verificava se o valor era maior, e alocava todos multiplos de 3 em um arrayList
                 maior = i;
                 //nao é necessario utilizar continue;
@@ -41,7 +41,7 @@ public class HobbitContador{
     
     public ArrayList<Integer> obterMultiplosDeTresAte(int numero){
         ArrayList<Integer> multiplos = new ArrayList<>(Arrays.asList(0));
-        for (int i = 3; i <= numero; i += 3){ //otimiza laço com i = 3 e somando mais 3 a i
+        for (int i = 3; i <= numero; i += 3){ //otimiza laço com i = 3 e somando mais 3 a i. Variavel utilizada como parametro invalida.
             if (i % 3 == 0) //break sairia do laço se i % 3 fosse zero
             multiplos.add(i);
         }
