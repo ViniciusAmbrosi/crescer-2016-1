eeba4ce6bdfce597e5e1246b9bb77dfa1ac8db48
Vinicius A <Vinicius A>
2016-04-11 23:45:57 -0300

    fix(ElfoNoturno): separa o algoritmo de perda de vida em um outro método, assim deixando os métodos com só uma função. Remove duplicação de código.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturno.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturno.java
@@ -8,21 +8,21 @@ public class ElfoNoturno extends Elfo{
         super(nome, flechas);
     }
     
-    /**Nao, elfos nao podem morrer atirando flechas. Pq sempre perdera 5% da vida atual,
-    logo, nunca morrendo*/
     public void atirarFlecha(Dwarf anao){
         if(status == Status.MORTO)
             return;
-        this.experiencia += 3;
-        this.flechas--;
-        anao.perdeVida();
-        double vidaAposFlechada = vida - 5;//vida menos 5% vida maxima
-        if(vidaAposFlechada == 0){
+        super.atirarFlecha(anao, 3);
+        this.perdeVida();
+    }
+    
+    /**Perdendo 5% da vida atual, os elfos não morrem ao lançar flechas, pois nunca perderão 100% da vida.*/
+    private void perdeVida(){
+        double vidaAposAtirarFlecha = vida - 5;//vida menos 5% vida maxima
+        if(vidaAposAtirarFlecha == 0){
             status = status.MORTO;
-            vida = 0;
         }
         if(vida > 0)
-            vida = vidaAposFlechada;
+            vida = vidaAposAtirarFlecha;
     }
-    /**Agora elfos morre, pois perdem 5% da vida total*/
+    /**Perdendo 5% da vida total, os elfos podem morrer ao lançar flechas.*/
 }
