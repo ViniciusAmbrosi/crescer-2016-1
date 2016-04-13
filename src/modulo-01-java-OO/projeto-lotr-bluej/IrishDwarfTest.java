d45a4d1d33372bffa509407d263734f9bc33770c
Vinicius A <Vinicius A>
2016-04-13 10:57:54 -0300

    feat(equals): adicionar método de equals na classe personagem.

--- src/modulo-01-java-OO/projeto-lotr-bluej/IrishDwarfTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/IrishDwarfTest.java
@@ -38,4 +38,74 @@ public class IrishDwarfTest{
         assertTrue(6003 == sortudo.getInventario().getItens().get(0).getQtd());
         assertTrue(15005 == sortudo.getInventario().getItens().get(1).getQtd());
     }
+    
+    @Test
+    public void dwarfsIguais(){ //retorna true
+        Dwarf d1 = new IrishDwarf("gimli");
+        Dwarf d2 = new IrishDwarf("gimli");
+        assertTrue(d1.equals(d2));
+    }
+    
+    @Test
+    public void dwarfsDiferentes(){ //returna false
+        Dwarf d1 = new IrishDwarf("gimli");
+        Dwarf d2 = new IrishDwarf("gertrude");
+        assertFalse(d1.equals(d2));
+    }
+    
+    @Test
+    public void dwarfsXpDiferente(){//retorna false
+        DataTerceiraEra dte = new DataTerceiraEra(1,1,2000);
+        Dwarf d1 = new IrishDwarf("gimli", dte);
+        for(int i = 0; i < 3; i++)    
+            d1.perdeVida();
+        Dwarf d2 = new IrishDwarf("gertrude");
+        assertFalse(d1.equals(d2));
+    }
+    
+    @Test
+    public void dwarfsInventarioDiferente(){//retorna false
+        Dwarf d1 = new IrishDwarf("gimli");
+        d1.adicionarItem(new Item(1, "adaga"));
+        Dwarf d2 = new IrishDwarf("gertrude");
+        d2.adicionarItem(new Item(2, "espada bastarda"));
+        assertFalse(d1.equals(d2));
+    }
+    
+    @Test
+    public void primeiroDwarfNomeNulo(){//retorna false
+        Dwarf d1 = new IrishDwarf(null);
+        Dwarf d2 = new IrishDwarf("gertrude");
+        assertFalse(d1.equals(d2));
+    }
+    
+    @Test
+    public void segundoDwarfNomeNulo(){//retorna false
+        Dwarf d1 = new IrishDwarf("gimli");
+        Dwarf d2 = new IrishDwarf(null);
+        assertFalse(d1.equals(d2));
+    }
+    
+    @Test
+    public void ambsoDwarfsNomeNulo(){//retorna true
+        Dwarf d1 = new IrishDwarf(null);
+        Dwarf d2 = new IrishDwarf(null);
+        assertTrue(d1.equals(d2));
+    }
+    
+    @Test
+    public void dwarfsStatusDiferente(){//retorna false
+        Dwarf d1 = new IrishDwarf("gimli");
+        Dwarf d2 = new IrishDwarf("gertrude");
+        for(int i = 0; i < 12; i++)
+            d1.perdeVida();
+        assertFalse(d1.equals(d2));
+    }
+      
+    @Test
+    public void dwarfsVidaDiferente(){//retorna false
+        Dwarf d1 = new IrishDwarf("gimli");
+        Dwarf d2 = new IrishDwarf("gertrude");
+        assertFalse(d1.equals(d2));
+    }
 }
--- src/modulo-01-java-OO/projeto-lotr-bluej/Personagem.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Personagem.java
@@ -21,6 +21,30 @@ public abstract class Personagem{
        this.inventario.removerItem(item);
     }
     
+    public boolean equals(Object obj){
+        if(this == obj)
+            return true;
+        if(obj == null) //this nunca vai ser nulo, já que equals nao permite
+            return false;
+        Personagem personagem = (Personagem) obj;
+        if(this.getClass() != personagem.getClass())
+            return false;
+        if(this.experiencia != personagem.experiencia)
+            return false;
+        if(!this.inventario.equals(personagem.inventario))
+            return false;
+        if(this.nome == null){
+             if(personagem.nome != null)
+                return false;
+        }else if(!this.nome.equals(personagem.nome))
+            return false;
+        if(this.status != personagem.status)
+            return false;
+        if(this.vida != personagem.vida)
+            return false;
+        return true;
+    }
+    
     public Inventario getInventario(){return this.inventario;}
     
     public String getNome(){return this.nome;}
