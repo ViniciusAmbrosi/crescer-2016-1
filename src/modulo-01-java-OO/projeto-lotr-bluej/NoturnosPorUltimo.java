06f54216a8db5e5d4b37487b6d0af75d3a0e531b
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-04-15 17:05:22 -0300

    refactor(NoturnosPorUltimo): adicionar algoritmo otmizado para deterimar chamada de ataque elfos verdes e noturnos.

--- src/modulo-01-java-OO/projeto-lotr-bluej/NoturnosPorUltimo.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/NoturnosPorUltimo.java
@@ -6,13 +6,27 @@ public class NoturnosPorUltimo implements Estrategia {
     public void atacar(HashMap<Status, ArrayList<Elfo>> exercitoElfos, ArrayList<Dwarf> dwarfs) {
         if (dwarfs == null || exercitoElfos.get(Status.VIVO) == null  || dwarfs.isEmpty())
             return;
-        ordemAtaqueElfos(new ElfoVerde("Verde"), exercitoElfos, dwarfs);
-        ordemAtaqueElfos(new ElfoNoturno("Noturno"), exercitoElfos, dwarfs);
+        Class previous = ElfoVerde.class;
+        Iterator<Elfo> it = exercitoElfos.get(Status.VIVO).iterator();
+        while(it.hasNext()){
+            Elfo aux = it.next();
+            if(aux.getClass().equals(previous)){
+                ordemAtaqueElfos(aux.getClass(), exercitoElfos, dwarfs);
+                if(previous.equals(ElfoNoturno.class))
+                    break;
+                previous = ElfoNoturno.class;
+                it = exercitoElfos.get(Status.VIVO).iterator();
+            }
+            if((!(it.hasNext())) && previous.equals(ElfoVerde.class)){
+                it = exercitoElfos.get(Status.VIVO).iterator();
+                previous = ElfoNoturno.class;
+            }
+        }
     }
 
-    private void ordemAtaqueElfos(Elfo tipo, HashMap<Status, ArrayList<Elfo>> exercitoElfos, ArrayList<Dwarf> dwarfs) {
+    private void ordemAtaqueElfos(Class tipo, HashMap<Status, ArrayList<Elfo>> exercitoElfos, ArrayList<Dwarf> dwarfs) {
         for (Elfo elfo : exercitoElfos.get(Status.VIVO)) {
-            if (elfo.getClass().equals(tipo.getClass())) {
+            if (elfo.getClass().equals(tipo)) {
                 ordemAtaque.add(elfo);
                 for (Dwarf dwarf : dwarfs)
                     elfo.atirarFlecha(dwarf);
