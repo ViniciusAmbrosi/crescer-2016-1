c360e808e080153761fcd6432b80d7136f81991d
Vinicius A <Vinicius A>
2016-04-15 11:27:24 -0300

    fix(AtaqueIntercalado): tratar elfos passados por nao atingirem condição.

--- src/modulo-01-java-OO/projeto-lotr-bluej/AtaqueIntercalado.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/AtaqueIntercalado.java
@@ -2,20 +2,14 @@ import java.util.*;
 
 public class AtaqueIntercalado implements Estrategia {
 	ArrayList<Elfo> ordemAtaque = new ArrayList<Elfo>();
+	ArrayList<Elfo> delayed = new ArrayList<>();
 
 	public ArrayList<Elfo> atacar(HashMap<Status, ArrayList<Elfo>> exercitoElfos, ArrayList<Dwarf> dwarfs) {
 		if (dwarfs == null || exercitoElfos.get(Status.VIVO) == null || dwarfs.isEmpty()
 				|| mesmaQuantidadeVerdesNoturnos(exercitoElfos))
 			return ordemAtaque;
-		Elfo previous = new Elfo("gambiarra?");
-		for (Elfo elfo : exercitoElfos.get(Status.VIVO)) {
-			if (!(elfo.getClass().equals(previous.getClass()))) {
-				previous = elfo;
-				for (Dwarf dwarf : dwarfs)
-					elfo.atirarFlecha(dwarf);
-				ordemAtaque.add(previous);
-			}
-		}
+		Elfo previous = new Elfo("previous");
+		ordenaAtaque(previous, dwarfs, exercitoElfos.get(Status.VIVO));
 		return ordemAtaque;
 	}
 
@@ -31,4 +25,30 @@ public class AtaqueIntercalado implements Estrategia {
 		return !(countVerde == countNoturno);
 	}
 
+	private void ordenaAtaque(Elfo previous, ArrayList<Dwarf> dwarfs, ArrayList<Elfo> elfos) {
+		Elfo elfoD = null;
+		for (Elfo elfo : elfos) {
+			if (!(elfo.getClass().equals(previous.getClass()))) {
+				elfoAtacaAnoes(elfo, dwarfs);
+				previous = elfo;
+				if (!delayed.isEmpty()) {
+					Iterator<Elfo> it = delayed.iterator();
+					while(it.hasNext())
+						elfoD =  it.next();
+						if (!(elfoD.getClass().equals(previous.getClass()))) {
+							elfoAtacaAnoes(elfoD, dwarfs);
+							previous = elfoD;
+							it.remove();
+					}
+				}
+			} else
+				delayed.add(elfo);
+		}
+	}
+
+	private void elfoAtacaAnoes(Elfo elfo, ArrayList<Dwarf> dwarfs) {
+		for (Dwarf dwarf : dwarfs)
+			elfo.atirarFlecha(dwarf);
+		ordemAtaque.add(elfo);
+	}
 }
