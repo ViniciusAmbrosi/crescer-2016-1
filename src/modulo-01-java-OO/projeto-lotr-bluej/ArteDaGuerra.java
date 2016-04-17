cb404d6384fd03526e8ee1157ff878c0781dde85
Vinicius A <Vinicius A>
2016-04-17 20:01:17 -0300

    refactor(ArteDaGuerra): retirar chamadas desnecessárias de get(status.VIVO) atribuindo resultado a uma variavel local.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ArteDaGuerra.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ArteDaGuerra.java
@@ -4,10 +4,11 @@ public class ArteDaGuerra implements Estrategia {
     ArrayList<Elfo> ordemAtaque = new ArrayList<Elfo>();
 
     public void atacar(HashMap<Status, ArrayList<Elfo>> exercitoElfos, ArrayList<Dwarf> dwarfs) {
-        if (dwarfs == null || exercitoElfos.get(Status.VIVO) == null || dwarfs.isEmpty())
+    	ArrayList<Elfo> exercitoElfosVivos = exercitoElfos.get(Status.VIVO);
+        if (dwarfs == null || exercitoElfosVivos == null || dwarfs.isEmpty())
             return;
-        int qtdNoturnos = quantosElfosNoturnosAtacam(exercitoElfos.get(Status.VIVO).size(), dwarfs.size());
-        for (Elfo elfo : exercitoElfos.get(Status.VIVO)) {
+        int qtdNoturnos = quantosElfosNoturnosAtacam(exercitoElfosVivos.size(), dwarfs.size());
+        for (Elfo elfo : exercitoElfosVivos) {
             boolean ehElfoNoturno = elfo instanceof ElfoNoturno;
             if (ehElfoNoturno && qtdNoturnos <= 0)
                 continue;
@@ -21,7 +22,7 @@ public class ArteDaGuerra implements Estrategia {
     }
 
     private int quantosElfosNoturnosAtacam(int elfoSize, int dwarfSize) {
-        return (int) Math.floor((elfoSize * dwarfSize) * 0.3);
+        return (int)(elfoSize * dwarfSize  * 0.3);
     }
     
     public ArrayList<Elfo> getOrdemDoUltimoAtaque(){
