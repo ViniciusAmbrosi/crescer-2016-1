25742f1b246d82e627bf0e21040b8131a8bd18ba
Vinicius A <Vinicius A>
2016-04-17 21:34:29 -0300

    fix(ExercitoDeElfos): retirar psvm da classe ExercitoDeElfos.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfos.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfos.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ExercitoDeElfos{
+public class ExercitoDeElfos {
 	private HashMap<String, Elfo> exercito = new HashMap<>();
 	private HashMap<Status, ArrayList<Elfo>> exercitoAgrupado = new HashMap<>();
 
@@ -36,84 +36,4 @@ public class ExercitoDeElfos{
 	public HashMap<String, Elfo> getExercito() {
 		return exercito;
 	}
-
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		ExercitoDeElfos ede = new ExercitoDeElfos();
-		ArrayList<Dwarf> hordaDwarfs = new ArrayList<>();
-		Estrategia strat = new ArteDaGuerra();;
-		int aux;
-		String stratType = "";
-		boolean cont = false;
-		try{
-		do{
-			//cria elfos noturnos
-			System.out.println("Quantos elfos Noturnos quer que o exercito tenha?");
-			aux = sc.nextInt();
-			for(int i = 0; i < aux; i++)
-				ede.alistaElfo(new ElfoNoturno("elfoNoturno"+i));
-			//cria elfos verdes
-			System.out.println("Quantos elfos Verdes quer que o exercito tenha?");
-			aux = sc.nextInt();
-			for(int i = 0; i < aux; i++)
-				ede.alistaElfo(new ElfoVerde("elfoVerde"+i));
-			//cria anoes
-			System.out.println("Qual o tamanho da horda de anoes?");
-			aux = sc.nextInt();
-			for(int i = 0; i < aux; i++)
-				hordaDwarfs.add(new Dwarf("dwarf"+i));
-			//limbo
-			ede.agrupaPorStatus();
-			sc.nextLine();
-			//define estrategia
-			boolean stratVal;
-			do{ 	
-				stratVal = true;
-				System.out.println(" Qual estrategia devera o general usar? \n"
-						+  "[adg]ArteDaGuerra [npu]NoturnosPorUltimo [ai]AtaqueIntercalado \n");
-				stratType = sc.nextLine(); 
-				switch(stratType.toLowerCase()){
-					case "adg":
-						strat = new ArteDaGuerra();
-						stratVal = false;
-						break;
-					case "npu":
-						strat = new NoturnosPorUltimo();
-						stratVal = false;
-						break;
-					case "ai":
-						strat = new AtaqueIntercalado();
-						stratVal = false;
-						break;
-					default:
-						throw new EstrategiaInvalidaException();
-				}
-				//ATACA
-				ede.atacar(strat, hordaDwarfs);
-				ArrayList<Elfo> atacaram = strat.getOrdemDoUltimoAtaque();
-				System.out.println("A ordem de ataque foi: ");
-				for(Elfo elfo : atacaram)
-					System.out.println(elfo.getClass() + " " + elfo.getNome());		
-				if(!stratVal)
-					System.out.println("Deseja fazer mais um ataque? [S]im ou qualquer tecla para encerrar");
-					stratType = sc.nextLine();
-					switch (stratType.toLowerCase()){
-						case "s":
-							stratVal = true;
-							break;
-						default:
-							break;
-					}
-					
-			}while(stratVal);
-		}while(cont);
-		sc.close();
-		}catch(InputMismatchException e){
-			System.out.println("Um dos valores inseridos foi invalido");
-		}catch(EstrategiaInvalidaException e){
-			System.out.println(e.toString());
-		}
-		
-	}
-
 }
