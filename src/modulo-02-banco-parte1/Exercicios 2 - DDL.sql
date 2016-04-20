8a8765274dc7f40e313e078cf660857d1e21813a
Vinicius A <Vinicius A>
2016-04-19 22:03:31 -0300

    feat(Exercicios2DDL): adicionar query do exercicio 2 dos slides de DDL.

---
+++ src/modulo-02-banco-parte1/Exercicios 2 - DDL.sql
@@ -0,0 +1,12 @@
+SELECT NomeEmpregado FROM Empregado
+ORDER BY DataAdmissao ASC
+
+SELECT NomeEmpregado, (Salario * 12) as SalarioAnual, Cargo FROM Empregado
+WHERE Cargo = 'Atendente' OR
+	  (Salario * 12) < 18500;
+
+SELECT IDCidade FROM Cidade
+WHERE Nome = 'Uberlândia';
+
+SELECT IDCidade, Nome FROM CIDADE
+WHERE UF = 'RS'
