a5bc75ba97b2e96d7b2df09ef0be2af3bca3fcfa
Vinicius A <Vinicius A>
2016-04-21 19:12:08 -0300

    style(Inventario): formatar, melhorar nome de variaveis,

--- src/modulo-01-java-OO/projeto-lotr-bluej/Inventario.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Inventario.java
@@ -47,36 +47,44 @@ public class Inventario {
 			item.adicionaItemProporcionalQuantidade();
 	}
 
-	private static boolean equals(ArrayList<Item> inv1, ArrayList<Item> inv2, int numExec , boolean equals) {
+	private static boolean equals(ArrayList<Item> inventarioUm, ArrayList<Item> inventarioDois, int numExecucoes,
+			boolean equals) {
 		// sai da recursividade
 		if (equals == false)
 			return false;
-		if (numExec == 1)
+		if (numExecucoes == 1)
 			return equals;
 		// valida
-		for (Item item : inv1) {
-			if (inv2.contains(item))
+		for (Item item : inventarioUm) {
+			if (inventarioDois.contains(item))
 				continue;
 			return false;
 		}
-		return equals(inv2, inv1, ++numExec, true);
+		return equals(inventarioDois, inventarioUm, ++numExecucoes, true);
 	}
-	/*Critério de igualdade é que ambos inventários devem possuir os mesmos itens, tendo eles a mesma quantidade e descrição (em uma mesma instância).
-	 * Para garantir a verdade da igualdade é necessârio comparar inventário A com invetário B e TAMBEM B com A. Pois se A contêm duas instâncias de X
-	 * (X e X) e b contém uma instância de X e uma de Y (X e Y), ao comparar A e B baseando-se somente em A.equals(B), seria determiado como igual. Mas tal 
-	 * inverdade é provada se comparado B com A.
+
+	/*
+	 * Critério de igualdade é que ambos inventários devem possuir os mesmos
+	 * itens, tendo eles a mesma quantidade e descrição (em uma mesma
+	 * instância). Para garantir a verdade da igualdade é necessârio comparar
+	 * inventário A com invetário B e TAMBEM B com A. Pois se A contêm duas
+	 * instâncias de X (X e X) e b contém uma instância de X e uma de Y (X e Y),
+	 * ao comparar A e B baseando-se somente em A.equals(B), seria determiado
+	 * como igual. Mas tal inverdade é provada se comparado B com A.
 	 */
 	public boolean equals(Object obj) {
-		int numExec = 0; //numero de vezes que executa recursão
+		int numExecucoes = 0; // numero de vezes que executa recursão
 		Inventario invCast = ((Inventario) this);
-		Inventario invCast2 = ((Inventario) obj);
-		ArrayList<Item> inv1 = invCast.itens;
-		ArrayList<Item> inv2 = invCast2.itens;
-		if (inv2 == null) // !!!inv2 pq inv1 NUNCA vai ser nulo para chamar equals
+		Inventario invCastDois = ((Inventario) obj);
+		ArrayList<Item> inventarioUm = invCast.itens;
+		ArrayList<Item> inventarioDois = invCastDois.itens;
+		// inventarioUm NUNCA vai ser nulo para chamar
+		if (inventarioDois == null)
 			return false;
-		if (inv1.size() != inv2.size()) // se nao tiverem mesmo tamanho nao podem ser iguais
+		// se nao tiverem mesmo tamanho nao podem ser iguais
+		if (inventarioUm.size() != inventarioDois.size())
 			return false;
-		return equals(inv1, inv2, numExec, true);
+		return equals(inventarioUm, inventarioDois, numExecucoes, true);
 	}
 
 	public ArrayList<Item> getItens() {
