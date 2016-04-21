4ec77f9b1b127fb13009e23166ba6576a1fe2990
Vinicius A <Vinicius A>
2016-04-21 19:25:24 -0300

    test(ElfoNoturnoTest): formatar.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturnoTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturnoTest.java
@@ -3,39 +3,38 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ElfoNoturnoTest{
-    @Before
-    public void tearDown() {
-        System.gc();
-        System.runFinalization();
-    }
-    
-    @Test
-    public void atiraFlechaAnaoTesteRetorna3XpEPerde5Vida(){
-        ElfoNoturno elfo = new ElfoNoturno("nome");
-        Dwarf dwarf = new Dwarf("nome");
-        elfo.atirarFlecha(dwarf);
-        assertEquals(3, elfo.getExperiencia());
-        assertTrue(elfo.getVida() == 95);
-    }
-    
-    @Test
-    public void atiraFlechaAnaoTesteRetorna5987369392383789Vida(){
-        ElfoNoturno elfo = new ElfoNoturno("nome");
-        Dwarf dwarf = new Dwarf("nome");
-        for(int i = 0; i < 10; i++)
-            elfo.atirarFlecha(dwarf);
-        assertEquals(59.87369392383786, elfo.getVida(), 0.00000000000000);
-    }
-    
-    @Test public void elfoNoturnoMorreSemVidaNegativa(){
-        ElfoNoturno elfo = new ElfoNoturno("nome");
-        Dwarf dwarf = new Dwarf("nome");
-        for(int i = 0; i <= 100; i++)
-            elfo.atirarFlecha(dwarf);
-        assertTrue(elfo.getStatus() == Status.MORTO);
-        assertTrue(elfo.getVida() == 0);
-    }
+public class ElfoNoturnoTest {
+	@Before
+	public void tearDown() {
+		System.gc();
+		System.runFinalization();
+	}
+
+	@Test
+	public void atiraFlechaAnaoTesteRetorna3XpEPerde5Vida() {
+		ElfoNoturno elfo = new ElfoNoturno("nome");
+		Dwarf dwarf = new Dwarf("nome");
+		elfo.atirarFlecha(dwarf);
+		assertEquals(3, elfo.getExperiencia());
+		assertTrue(elfo.getVida() == 95);
+	}
+
+	@Test
+	public void atiraFlechaAnaoTesteRetorna5987369392383789Vida() {
+		ElfoNoturno elfo = new ElfoNoturno("nome");
+		Dwarf dwarf = new Dwarf("nome");
+		for (int i = 0; i < 10; i++)
+			elfo.atirarFlecha(dwarf);
+		assertEquals(59.87369392383786, elfo.getVida(), 0.00000000000000);
+	}
+
+	@Test
+	public void elfoNoturnoMorreSemVidaNegativa() {
+		ElfoNoturno elfo = new ElfoNoturno("nome");
+		Dwarf dwarf = new Dwarf("nome");
+		for (int i = 0; i <= 100; i++)
+			elfo.atirarFlecha(dwarf);
+		assertTrue(elfo.getStatus() == Status.MORTO);
+		assertTrue(elfo.getVida() == 0);
+	}
 }
-
-
