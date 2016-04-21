fb0ca21add3661e49ee45be4b35645cb21f44118
Vinicius A <Vinicius A>
2016-04-21 19:24:45 -0300

    test(InventarioTest): formatar, melhorar nome variaveis.

--- src/modulo-01-java-OO/projeto-lotr-bluej/InventarioTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/InventarioTest.java
@@ -3,182 +3,182 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class InventarioTest{
-    @Test
-    public void criaInventarioEAddItem(){
-        Inventario inv = new Inventario();
-        Item item = new Item(1,"espada");
-        inv.addItem(item);
-        assertEquals(item, inv.getItens().get(0));
-    }
+public class InventarioTest {
+	@Test
+	public void criaInventarioEAddItem() {
+		Inventario inventario = new Inventario();
+		Item item = new Item(1, "espada");
+		inventario.addItem(item);
+		assertEquals(item, inventario.getItens().get(0));
+	}
 
-    @Test
-    public void itemEspadaFoiRemovido(){
-        Inventario inv = new Inventario();
-        Item espada = new Item(1,"espada");
-        Item arco = new Item(1,"arco");
-        Item escudo = new Item(1,"escudo");
-        inv.addItem(espada);
-        inv.addItem(arco);
-        inv.addItem(escudo);
-        inv.removerItem(espada);
-        assertEquals(arco, inv.getItens().get(0));
-    }
-    
-    @Test
-    public void descreveEspadaAdagaArco(){
-        Inventario inv = new Inventario();
-        inv.addItem(new Item(1, "Espada"));
-        inv.addItem(new Item(1, "Adaga"));
-        inv.addItem(new Item(1, "Arco"));
-        boolean descreveEspadaAdagaArco = "Espada,Adaga,Arco".equals(inv.getDescricaoItens());
-        assertTrue(descreveEspadaAdagaArco);
-    }
-    
-    @Test
-    public void espadaItemMaisComum(){
-        Inventario inv = new Inventario();
-        Item espada = new Item(5,"Espada");
-        inv.addItem(espada);
-        inv.addItem(new Item(1, "Adaga"));
-        inv.addItem(new Item(1, "Arco"));
-        assertTrue(inv.getItemMaisComum() == espada);
-    }
-    
-    @Test
-    public void ordenaItensArcoAdagaEspada(){
-        Inventario inv = new Inventario();
-        Item espada = new Item(3, "Espada");
-        Item adaga = new Item(2, "Adaga");
-        Item arco = new Item(1, "Arco");
-        inv.addItem(espada);
-        inv.addItem(adaga);
-        inv.addItem(arco);
-        inv.ordenarItens();
-        assertTrue(inv.getItens().get(0) == arco);
-        assertTrue(inv.getItens().get(1) == adaga);
-        assertTrue(inv.getItens().get(2) == espada);
-    }
-    
-    @Test
-    public void ordenaItensArcoEspadaAdaga(){
-        Inventario inv = new Inventario();
-        Item espada = new Item(2, "Espada");
-        Item adaga = new Item(3, "Adaga");
-        Item arco = new Item(1, "Arco");
-        inv.addItem(espada);
-        inv.addItem(adaga);
-        inv.addItem(arco);
-        inv.ordenarItens();
-        assertTrue(inv.getItens().get(0) == arco);
-        assertTrue(inv.getItens().get(2) == adaga);
-        assertTrue(inv.getItens().get(1) == espada);
-    }
-    
-    @Test
-    public void ordenaItensAdagaEspadaArco(){
-        Inventario inv = new Inventario();
-        Item espada = new Item(2, "Espada");
-        Item adaga = new Item(1, "Adaga");
-        Item arco = new Item(3, "Arco");
-        inv.addItem(espada);
-        inv.addItem(adaga);
-        inv.addItem(arco);
-        inv.ordenarItens();
-        assertTrue(inv.getItens().get(2) == arco);
-        assertTrue(inv.getItens().get(0) == adaga);
-        assertTrue(inv.getItens().get(1) == espada);
-    }
-    
-    public void primeiroInventarioNulo(){
-        Inventario inv1 = null;
-        Inventario inv2 = new Inventario();
-        Item item1 = new Item(2, "Poção");
-        inv2.addItem(item1);
-        assertTrue(inv1.equals(inv2));
-    }
-    
-    public void segundoInventarioNulo(){
-        Inventario inv1 = new Inventario();
-        Inventario inv2 = null;
-        Item item1 = new Item(1, "Espada");
-        inv1.addItem(item1);
-        assertTrue(inv1.equals(inv2));
-    }
-    
-    public void ambosInventariosNulos(){
-        Inventario inv1 = null;
-        Inventario inv2 = null;
-        assertTrue(inv1.equals(inv2));
-    
-    }
-    
-    public void primeiroInventarioItemNulo(){
-        Inventario inv1 = new Inventario();
-        Inventario inv2 = new Inventario();
-        Item item1 = null;
-        Item item2 = new Item(2, "Poção");
-        inv1.addItem(item1);
-        inv2.addItem(item2);
-        assertTrue(inv1.equals(inv2));
-    }
-    
-    public void segundoInventarioItemNulo(){
-        Inventario inv1 = new Inventario();
-        Inventario inv2 = new Inventario();
-        Item item1 = new Item(1, "Poção");
-        Item item2 = null;
-        inv1.addItem(item1);
-        inv2.addItem(item2);
-        assertTrue(inv1.equals(inv2));
-    }
-    
-    public void ambosInventariosItensNulos(){
-        Inventario inv1 = new Inventario();
-        Inventario inv2 = new Inventario();
-        Item item1 = null;
-        Item item2 = null;
-        inv1.addItem(item1);
-        inv2.addItem(item2);
-        assertTrue(inv1.equals(inv2));
-    }
-    
-    public void inventariosDiferentes(){
-        Inventario inv1 = new Inventario();
-        Inventario inv2 = new Inventario();
-        Item item1 = new Item(3, "Poção");
-        Item item2 = new Item(2, "Poção");
-        inv1.addItem(item1);
-        inv2.addItem(item2);
-        assertTrue(inv1.equals(inv2));
-    }
-    
-    @Test
-    public void inventariosIguais(){
-        Inventario inv1 = new Inventario();
-        Inventario inv2 = new Inventario();
-        Item item1 = new Item(1, "Espada");
-        Item item2 = new Item(2, "Poção");
-        Item item3 = new Item(1, "Espada");
-        Item item4 = new Item(2, "Poção");
-        inv1.addItem(item1);
-        inv2.addItem(item3);
-        inv1.addItem(item2);
-        inv2.addItem(item4);
-        assertTrue(inv1.equals(inv2));
-    }
-    
-    @Test
-    public void inventarioComObjetoDuplicado(){
-        Inventario inv1 = new Inventario();
-        Inventario inv2 = new Inventario();
-        Item item1 = new Item(1, "Espada");
-        Item item2 = new Item(2, "Poção");
-        inv1.addItem(item1);
-        inv1.addItem(item1);
-        inv2.addItem(item1);
-        inv2.addItem(item2);
-        assertTrue(inv1.equals(inv2));
-    }
-    
+	@Test
+	public void itemEspadaFoiRemovido() {
+		Inventario inventario = new Inventario();
+		Item espada = new Item(1, "espada");
+		Item arco = new Item(1, "arco");
+		Item escudo = new Item(1, "escudo");
+		inventario.addItem(espada);
+		inventario.addItem(arco);
+		inventario.addItem(escudo);
+		inventario.removerItem(espada);
+		assertEquals(arco, inventario.getItens().get(0));
+	}
+
+	@Test
+	public void descreveEspadaAdagaArco() {
+		Inventario inventario = new Inventario();
+		inventario.addItem(new Item(1, "Espada"));
+		inventario.addItem(new Item(1, "Adaga"));
+		inventario.addItem(new Item(1, "Arco"));
+		boolean descreveEspadaAdagaArco = "Espada,Adaga,Arco".equals(inventario.getDescricaoItens());
+		assertTrue(descreveEspadaAdagaArco);
+	}
+
+	@Test
+	public void espadaItemMaisComum() {
+		Inventario inventario = new Inventario();
+		Item espada = new Item(5, "Espada");
+		inventario.addItem(espada);
+		inventario.addItem(new Item(1, "Adaga"));
+		inventario.addItem(new Item(1, "Arco"));
+		assertTrue(inventario.getItemMaisComum() == espada);
+	}
+
+	@Test
+	public void ordenaItensArcoAdagaEspada() {
+		Inventario inventario = new Inventario();
+		Item espada = new Item(3, "Espada");
+		Item adaga = new Item(2, "Adaga");
+		Item arco = new Item(1, "Arco");
+		inventario.addItem(espada);
+		inventario.addItem(adaga);
+		inventario.addItem(arco);
+		inventario.ordenarItens();
+		assertTrue(inventario.getItens().get(0) == arco);
+		assertTrue(inventario.getItens().get(1) == adaga);
+		assertTrue(inventario.getItens().get(2) == espada);
+	}
+
+	@Test
+	public void ordenaItensArcoEspadaAdaga() {
+		Inventario inventario = new Inventario();
+		Item espada = new Item(2, "Espada");
+		Item adaga = new Item(3, "Adaga");
+		Item arco = new Item(1, "Arco");
+		inventario.addItem(espada);
+		inventario.addItem(adaga);
+		inventario.addItem(arco);
+		inventario.ordenarItens();
+		assertTrue(inventario.getItens().get(0) == arco);
+		assertTrue(inventario.getItens().get(2) == adaga);
+		assertTrue(inventario.getItens().get(1) == espada);
+	}
+
+	@Test
+	public void ordenaItensAdagaEspadaArco() {
+		Inventario inventario = new Inventario();
+		Item espada = new Item(2, "Espada");
+		Item adaga = new Item(1, "Adaga");
+		Item arco = new Item(3, "Arco");
+		inventario.addItem(espada);
+		inventario.addItem(adaga);
+		inventario.addItem(arco);
+		inventario.ordenarItens();
+		assertTrue(inventario.getItens().get(2) == arco);
+		assertTrue(inventario.getItens().get(0) == adaga);
+		assertTrue(inventario.getItens().get(1) == espada);
+	}
+
+	public void primeiroInventarioNulo() {
+		Inventario inventarioUm = null;
+		Inventario inventarioDois = new Inventario();
+		Item itemUm = new Item(2, "Pocao");
+		inventarioDois.addItem(itemUm);
+		assertTrue(inventarioUm.equals(inventarioDois));
+	}
+
+	public void segundoInventarioNulo() {
+		Inventario inventarioUm = new Inventario();
+		Inventario inventarioDois = null;
+		Item itemUm = new Item(1, "Espada");
+		inventarioUm.addItem(itemUm);
+		assertTrue(inventarioUm.equals(inventarioDois));
+	}
+
+	public void ambosInventariosNulos() {
+		Inventario inventarioUm = null;
+		Inventario inventarioDois = null;
+		assertTrue(inventarioUm.equals(inventarioDois));
+
+	}
+
+	public void primeiroInventarioItemNulo() {
+		Inventario inventarioUm = new Inventario();
+		Inventario inventarioDois = new Inventario();
+		Item itemUm = null;
+		Item itemDois = new Item(2, "Pocao");
+		inventarioUm.addItem(itemUm);
+		inventarioDois.addItem(itemDois);
+		assertTrue(inventarioUm.equals(inventarioDois));
+	}
+
+	public void segundoInventarioItemNulo() {
+		Inventario inventarioUm = new Inventario();
+		Inventario inventarioDois = new Inventario();
+		Item itemUm = new Item(1, "Pocao");
+		Item itemDois = null;
+		inventarioUm.addItem(itemUm);
+		inventarioDois.addItem(itemDois);
+		assertTrue(inventarioUm.equals(inventarioDois));
+	}
+
+	public void ambosInventariosItensNulos() {
+		Inventario inventarioUm = new Inventario();
+		Inventario inventarioDois = new Inventario();
+		Item itemUm = null;
+		Item itemDois = null;
+		inventarioUm.addItem(itemUm);
+		inventarioDois.addItem(itemDois);
+		assertTrue(inventarioUm.equals(inventarioDois));
+	}
+
+	public void inventariosDiferentes() {
+		Inventario inventarioUm = new Inventario();
+		Inventario inventarioDois = new Inventario();
+		Item itemUm = new Item(3, "Pocao");
+		Item itemDois = new Item(2, "Pocao");
+		inventarioUm.addItem(itemUm);
+		inventarioDois.addItem(itemDois);
+		assertTrue(inventarioUm.equals(inventarioDois));
+	}
+
+	@Test
+	public void inventariosIguais() {
+		Inventario inventarioUm = new Inventario();
+		Inventario inventarioDois = new Inventario();
+		Item itemUm = new Item(1, "Espada");
+		Item itemDois = new Item(2, "Pocao");
+		Item itemTres = new Item(1, "Espada");
+		Item itemQuatro = new Item(2, "Pocao");
+		inventarioUm.addItem(itemUm);
+		inventarioDois.addItem(itemTres);
+		inventarioUm.addItem(itemDois);
+		inventarioDois.addItem(itemQuatro);
+		assertTrue(inventarioUm.equals(inventarioDois));
+	}
+
+	@Test
+	public void inventarioComObjetoDuplicado() {
+		Inventario inventarioUm = new Inventario();
+		Inventario inventarioDois = new Inventario();
+		Item itemUm = new Item(1, "Espada");
+		Item itemDois = new Item(2, "Pocao");
+		inventarioUm.addItem(itemUm);
+		inventarioUm.addItem(itemUm);
+		inventarioDois.addItem(itemUm);
+		inventarioDois.addItem(itemDois);
+		assertTrue(inventarioUm.equals(inventarioDois));
+	}
+
 }
