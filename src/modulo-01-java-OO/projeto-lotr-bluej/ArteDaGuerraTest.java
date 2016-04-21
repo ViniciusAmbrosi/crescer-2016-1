3eb15fc2ec1dae33b1572f2a593aba14dba07f12
Vinicius A <Vinicius A>
2016-04-21 19:19:34 -0300

    test(ArteDaGuerra): modificar método de validação para verificar ordem de elementos, adicionar cenários com ElfoNoturno sendo criado primeiro que ElfoVerde.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ArteDaGuerraTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ArteDaGuerraTest.java
@@ -5,13 +5,24 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ArteDaGuerraTest {
-	
-    @Before
-    public void tearDown() {
-        System.gc();
-        System.runFinalization();
-    }
-    
+
+	@Before
+	public void tearDown() {
+		System.gc();
+		System.runFinalization();
+	}
+
+	/* já recebe valor de elfos esperados em cima de intençoes de atk */
+	private boolean ordemAtqEhValida(ArrayList<Elfo> ordem, double totalElfosNoturnosEsperados) {
+		int elfosNoturnosAtacaram = 0;
+		for (Elfo elfo : ordem)
+			if (elfo instanceof ElfoNoturno)
+				elfosNoturnosAtacaram++;
+		if (elfosNoturnosAtacaram == totalElfosNoturnosEsperados)
+			return true;
+		else
+			return false;
+	}
 
 	private ArrayList<Dwarf> factoryDwarfs(int anoes) {
 		ArrayList<Dwarf> dwarfs = new ArrayList<>();
@@ -20,19 +31,28 @@ public class ArteDaGuerraTest {
 		return dwarfs;
 	}
 
-	private ExercitoDeElfos factoryElfos(int elfosVerdes, int elfosNoturnos) {
-		ExercitoDeElfos ede = new ExercitoDeElfos();
+	private ExercitoDeElfos factoryElfosVerdesPrimeiro(int elfosVerdes, int elfosNoturnos) {
+		ExercitoDeElfos exercitoDeElfos = new ExercitoDeElfos();
 		for (int i = 0; i < elfosVerdes; i++)
-			ede.alistaElfo(new ElfoVerde("ElfoVerde" + i));
+			exercitoDeElfos.alistaElfo(new ElfoVerde("ElfoVerde" + i));
 		for (int i = 0; i < elfosNoturnos; i++)
-			ede.alistaElfo(new ElfoNoturno("ElfoNortuno" + i));
-		return ede;
+			exercitoDeElfos.alistaElfo(new ElfoNoturno("ElfoNortuno" + i));
+		return exercitoDeElfos;
 	}
 
-	private void mataElfos(ExercitoDeElfos ede, int mataQuantos) {
-		DataTerceiraEra dte = new DataTerceiraEra(1, 1, 2000);
-		Dwarf dwarf = new Dwarf("Imortal", dte);
-		for (Elfo elfo : ede.getExercito().values()) {
+	private ExercitoDeElfos factoryElfosNoturnosPrimeiro(int elfosNoturnos, int elfosVerdes) {
+		ExercitoDeElfos exercitoDeElfos = new ExercitoDeElfos();
+		for (int i = 0; i < elfosNoturnos; i++)
+			exercitoDeElfos.alistaElfo(new ElfoNoturno("ElfoNoturno" + i));
+		for (int i = 0; i < elfosVerdes; i++)
+			exercitoDeElfos.alistaElfo(new ElfoVerde("ElfoVerde" + i));
+		return exercitoDeElfos;
+	}
+
+	private void mataElfos(ExercitoDeElfos exercitoDeElfos, int mataQuantos) {
+		DataTerceiraEra dataTerceiraEra = new DataTerceiraEra(1, 1, 2000);
+		Dwarf dwarf = new Dwarf("Imortal", dataTerceiraEra);
+		for (Elfo elfo : exercitoDeElfos.getExercito().values()) {
 			if (mataQuantos-- <= 0)
 				break;
 			for (int i = 0; i < 92; i++)
@@ -43,134 +63,268 @@ public class ArteDaGuerraTest {
 	@Test
 	public void dezElfosAtacam6Anoes() {
 		Estrategia strat = new ArteDaGuerra();
-		ExercitoDeElfos ede = factoryElfos(7, 3);
+		ExercitoDeElfos exercitoDeElfos = factoryElfosVerdesPrimeiro(7, 3);
 		ArrayList<Dwarf> dwarfs = factoryDwarfs(6);
-		ede.agrupaPorStatus();
-	    strat.atacar(ede.getExercitoAgrupado(), dwarfs); 		
-	    ArrayList<Elfo> ordemAtq = strat.getOrdemDoUltimoAtaque();
-		assertEquals(10, ordemAtq.size());
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 3));
 	}
 
 	@Test
 	public void dezElfosNoturnosAtacam6Anoes() {
 		Estrategia strat = new ArteDaGuerra();
-		ExercitoDeElfos ede = factoryElfos(0, 10);
+		ExercitoDeElfos exercitoDeElfos = factoryElfosVerdesPrimeiro(0, 10);
 		ArrayList<Dwarf> dwarfs = factoryDwarfs(6);
-		ede.agrupaPorStatus();
-		strat.atacar(ede.getExercitoAgrupado(), dwarfs); 		
-		ArrayList<Elfo> ordemAtq = strat.getOrdemDoUltimoAtaque();
-		assertEquals(3, ordemAtq.size());
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 3));
 	}
 
 	@Test
 	public void cincoElfosVerdesCincoElfoNoturnoAtacaUmAnao() {
 		Estrategia strat = new ArteDaGuerra();
-		ExercitoDeElfos ede = factoryElfos(5, 5);
+		ExercitoDeElfos exercitoDeElfos = factoryElfosVerdesPrimeiro(5, 5);
 		ArrayList<Dwarf> dwarfs = factoryDwarfs(1);
-		ede.agrupaPorStatus();
-		strat.atacar(ede.getExercitoAgrupado(), dwarfs); 		
-		ArrayList<Elfo> ordemAtq = strat.getOrdemDoUltimoAtaque();
-		assertEquals(8, ordemAtq.size());
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 3));
 	}
 
 	@Test
 	public void passaListaDwarfNulo() {
 		Estrategia strat = new ArteDaGuerra();
-		ExercitoDeElfos ede = factoryElfos(5, 5);
+		ExercitoDeElfos exercitoDeElfos = factoryElfosVerdesPrimeiro(5, 5);
 		ArrayList<Dwarf> dwarfs = null;
-		ede.agrupaPorStatus();
-		strat.atacar(ede.getExercitoAgrupado(), dwarfs); 
-		ArrayList<Elfo> ordemAtq = strat.getOrdemDoUltimoAtaque();
-		assertEquals(0, ordemAtq.size());
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 0));
 	}
 
 	@Test
 	public void passaExercitoVazio() {
 		Estrategia strat = new ArteDaGuerra();
-		ExercitoDeElfos ede = factoryElfos(0, 0);
+		ExercitoDeElfos exercitoDeElfos = factoryElfosVerdesPrimeiro(0, 0);
 		ArrayList<Dwarf> dwarfs = factoryDwarfs(1);
-		ede.agrupaPorStatus();
-		strat.atacar(ede.getExercitoAgrupado(), dwarfs); 
-		ArrayList<Elfo> ordemAtq = strat.getOrdemDoUltimoAtaque();
-		assertEquals(0, ordemAtq.size());
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 0));
 	}
 
 	@Test
 	public void passa0Anoes() {
 		Estrategia strat = new ArteDaGuerra();
-		ExercitoDeElfos ede = factoryElfos(10, 10);
+		ExercitoDeElfos exercitoDeElfos = factoryElfosVerdesPrimeiro(10, 10);
 		ArrayList<Dwarf> dwarfs = factoryDwarfs(0);
-		ede.agrupaPorStatus();
-		strat.atacar(ede.getExercitoAgrupado(), dwarfs); 
-		ArrayList<Elfo> ordemAtq = strat.getOrdemDoUltimoAtaque();
-		assertEquals(0, ordemAtq.size());
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 0));
 	}
 
 	@Test
 	public void passaUmElfoNulo() {
 		Estrategia strat = new ArteDaGuerra();
-		ExercitoDeElfos ede = factoryElfos(7, 3);
-		ede.alistaElfo(null);
+		ExercitoDeElfos exercitoDeElfos = factoryElfosVerdesPrimeiro(7, 3);
+		exercitoDeElfos.alistaElfo(null);
 		ArrayList<Dwarf> dwarfs = factoryDwarfs(5);
-		ede.agrupaPorStatus();
-		strat.atacar(ede.getExercitoAgrupado(), dwarfs); 	
-		ArrayList<Elfo> ordemAtq = strat.getOrdemDoUltimoAtaque();
-		assertEquals(10, ordemAtq.size());
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 3));
 	}
 
 	@Test
 	public void duzentosElfosNoturnosTentamAtacar() {
 		Estrategia strat = new ArteDaGuerra();
-		ExercitoDeElfos ede = factoryElfos(1, 200);
+		ExercitoDeElfos exercitoDeElfos = factoryElfosVerdesPrimeiro(1, 200);
 		ArrayList<Dwarf> dwarfs = factoryDwarfs(1);
-		ede.agrupaPorStatus();
-		strat.atacar(ede.getExercitoAgrupado(), dwarfs); 	
-		ArrayList<Elfo> ordemAtq = strat.getOrdemDoUltimoAtaque();
-		assertEquals(61, ordemAtq.size());
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 60));
 	}
 
 	@Test
 	public void nenhumElfoNoturnoAtaca() {
 		Estrategia strat = new ArteDaGuerra();
-		ExercitoDeElfos ede = factoryElfos(1, 1);
+		ExercitoDeElfos exercitoDeElfos = factoryElfosVerdesPrimeiro(1, 1);
 		ArrayList<Dwarf> dwarfs = factoryDwarfs(1);
-		ede.agrupaPorStatus();
-		strat.atacar(ede.getExercitoAgrupado(), dwarfs); 	
-		ArrayList<Elfo> ordemAtq = strat.getOrdemDoUltimoAtaque();
-		assertEquals(1, ordemAtq.size());
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 0));
 	}
 
 	@Test
 	public void zeroElfosVerdes() {
 		Estrategia strat = new ArteDaGuerra();
-		ExercitoDeElfos ede = factoryElfos(0, 5);
+		ExercitoDeElfos exercitoDeElfos = factoryElfosVerdesPrimeiro(0, 5);
 		ArrayList<Dwarf> dwarfs = factoryDwarfs(5);
-		ede.agrupaPorStatus();
-		strat.atacar(ede.getExercitoAgrupado(), dwarfs); 	
-		ArrayList<Elfo> ordemAtq = strat.getOrdemDoUltimoAtaque();
-		assertEquals(2, ordemAtq.size());
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 2));
 	}
 
 	@Test
 	public void zeroElfosNoturnos() {
 		Estrategia strat = new ArteDaGuerra();
-		ExercitoDeElfos ede = factoryElfos(5, 0);
+		ExercitoDeElfos exercitoDeElfos = factoryElfosVerdesPrimeiro(5, 0);
 		ArrayList<Dwarf> dwarfs = factoryDwarfs(5);
-		ede.agrupaPorStatus();
-		strat.atacar(ede.getExercitoAgrupado(), dwarfs);
-		ArrayList<Elfo> ordemAtq = strat.getOrdemDoUltimoAtaque();
-		assertEquals(5, ordemAtq.size());
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 0));
 	}
 
 	@Test
 	public void cincoElfosMortos() {
 		Estrategia strat = new ArteDaGuerra();
-		ExercitoDeElfos ede = factoryElfos(0, 15);
-		mataElfos(ede, 5);
+		ExercitoDeElfos exercitoDeElfos = factoryElfosVerdesPrimeiro(0, 15);
+		mataElfos(exercitoDeElfos, 5);
 		ArrayList<Dwarf> dwarfs = factoryDwarfs(5);
-		ede.agrupaPorStatus();
-		strat.atacar(ede.getExercitoAgrupado(), dwarfs); 		
-		ArrayList<Elfo> ordemAtq = strat.getOrdemDoUltimoAtaque();
-		assertEquals(3, ordemAtq.size());
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 3));
+	}
+
+	@Test
+	public void dezElfosAtacam6AnoesNoturnosPrimeiro() {
+		Estrategia strat = new ArteDaGuerra();
+		ExercitoDeElfos exercitoDeElfos = factoryElfosNoturnosPrimeiro(3, 7);
+		ArrayList<Dwarf> dwarfs = factoryDwarfs(6);
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 3));
+	}
+
+	@Test
+	public void dezElfosNoturnosAtacam6AnoesNoturnosPrimeiro() {
+		Estrategia strat = new ArteDaGuerra();
+		ExercitoDeElfos exercitoDeElfos = factoryElfosNoturnosPrimeiro(10, 0);
+		ArrayList<Dwarf> dwarfs = factoryDwarfs(6);
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 3));
+	}
+
+	@Test
+	public void cincoElfosVerdesCincoElfoNoturnoAtacaUmAnaoNoturnosPrimeiro() {
+		Estrategia strat = new ArteDaGuerra();
+		ExercitoDeElfos exercitoDeElfos = factoryElfosNoturnosPrimeiro(5, 5);
+		ArrayList<Dwarf> dwarfs = factoryDwarfs(1);
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 3));
+	}
+
+	@Test
+	public void passaListaDwarfNuloNoturnosPrimeiro() {
+		Estrategia strat = new ArteDaGuerra();
+		ExercitoDeElfos exercitoDeElfos = factoryElfosNoturnosPrimeiro(5, 5);
+		ArrayList<Dwarf> dwarfs = null;
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 0));
+	}
+
+	@Test
+	public void passaExercitoVazioNoturnosPrimeiro() {
+		Estrategia strat = new ArteDaGuerra();
+		ExercitoDeElfos exercitoDeElfos = factoryElfosNoturnosPrimeiro(0, 0);
+		ArrayList<Dwarf> dwarfs = factoryDwarfs(1);
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 0));
+	}
+
+	@Test
+	public void passa0AnoesNoturnosPrimeiro() {
+		Estrategia strat = new ArteDaGuerra();
+		ExercitoDeElfos exercitoDeElfos = factoryElfosNoturnosPrimeiro(10, 10);
+		ArrayList<Dwarf> dwarfs = factoryDwarfs(0);
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 0));
+	}
+
+	@Test
+	public void passaUmElfoNuloNoturnosPrimeiro() {
+		Estrategia strat = new ArteDaGuerra();
+		ExercitoDeElfos exercitoDeElfos = factoryElfosNoturnosPrimeiro(3, 7);
+		exercitoDeElfos.alistaElfo(null);
+		ArrayList<Dwarf> dwarfs = factoryDwarfs(5);
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 3));
+	}
+
+	@Test
+	public void duzentosElfosNoturnosTentamAtacarNoturnosPrimeiro() {
+		Estrategia strat = new ArteDaGuerra();
+		ExercitoDeElfos exercitoDeElfos = factoryElfosNoturnosPrimeiro(200, 1);
+		ArrayList<Dwarf> dwarfs = factoryDwarfs(1);
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 60));
+	}
+
+	@Test
+	public void nenhumElfoNoturnoAtacaNoturnosPrimeiro() {
+		Estrategia strat = new ArteDaGuerra();
+		ExercitoDeElfos exercitoDeElfos = factoryElfosNoturnosPrimeiro(1, 1);
+		ArrayList<Dwarf> dwarfs = factoryDwarfs(1);
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 0));
+	}
+
+	@Test
+	public void zeroElfosVerdesNoturnosPrimeiro() {
+		Estrategia strat = new ArteDaGuerra();
+		ExercitoDeElfos exercitoDeElfos = factoryElfosNoturnosPrimeiro(5, 0);
+		ArrayList<Dwarf> dwarfs = factoryDwarfs(5);
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 2));
+	}
+
+	@Test
+	public void zeroElfosNoturnosNoturnosPrimeiro() {
+		Estrategia strat = new ArteDaGuerra();
+		ExercitoDeElfos exercitoDeElfos = factoryElfosNoturnosPrimeiro(0, 5);
+		ArrayList<Dwarf> dwarfs = factoryDwarfs(5);
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 0));
+	}
+
+	@Test
+	public void cincoElfosMortosNoturnosPrimeiro() {
+		Estrategia strat = new ArteDaGuerra();
+		ExercitoDeElfos exercitoDeElfos = factoryElfosNoturnosPrimeiro(15, 0);
+		mataElfos(exercitoDeElfos, 5);
+		ArrayList<Dwarf> dwarfs = factoryDwarfs(5);
+		exercitoDeElfos.agrupaPorStatus();
+		strat.atacar(exercitoDeElfos.getExercitoAgrupado(), dwarfs);
+		ArrayList<Elfo> ordemAtaque = strat.getOrdemDoUltimoAtaque();
+		assertTrue(ordemAtqEhValida(ordemAtaque, 3));
 	}
 }
