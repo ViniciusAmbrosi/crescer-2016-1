ae036f91299f9050f11d3264649174c166dcfb80
Vinicius A <Vinicius A>
2016-04-21 19:23:14 -0300

    test(ElfoVerdeTest): formatar, melhorar nome variaveis.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoVerdeTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoVerdeTest.java
@@ -3,44 +3,42 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ElfoVerdeTest{
-    @Before
-    public void tearDown() {
-        System.gc();
-        System.runFinalization();
-    }
-    
-    @Test
-    public void adicionaEspadaDeAcoValiriano(){
-        ElfoVerde elfo = new ElfoVerde("legolas");
-        Item item = new Item(1, "Espada de aço valiriano");
-        elfo.adicionarItem(item);
-        assertTrue(elfo.getInventario().getItens().get(0).getDescricao().equals("Espada de aço valiriano"));
-    }
-    
-    @Test
-    public void ganha2XpAoAtirarFlecha(){
-        ElfoVerde elfo = new ElfoVerde("legolas");
-        Dwarf dwarf = new Dwarf("Gimli");
-        elfo.atirarFlecha(dwarf);
-        assertTrue(elfo.getExperiencia() == 2);
-    }
-    
-    @Test
-    public void retornaFalseTentativaAdicionarEspadaDeAcoValirian(){
-        ElfoVerde elfo = new ElfoVerde("legolas");
-        Item item = new Item(1, "Espada de aço valirian");
-        elfo.adicionarItem(item);
-        assertFalse(elfo.getInventario().getItens().contains(item));
-    }
-    
-    @Test
-    public void retornaFalseTentaAdicionarItemNull(){
-        ElfoVerde elfo = new ElfoVerde("legolas");
-        Item item = null;
-        elfo.adicionarItem(item);
-        assertFalse(elfo.getInventario().getItens().contains(item));
-    }
+public class ElfoVerdeTest {
+	@Before
+	public void tearDown() {
+		System.gc();
+		System.runFinalization();
+	}
+
+	@Test
+	public void adicionaEspadaDeAcoValiriano() {
+		ElfoVerde elfo = new ElfoVerde("legolas");
+		Item item = new Item(1, "Espada de aço valiriano");
+		elfo.adicionarItem(item);
+		assertTrue(elfo.getInventario().getItens().get(0).getDescricao().equals("Espada de aço valiriano"));
+	}
+
+	@Test
+	public void ganha2XpAoAtirarFlecha() {
+		ElfoVerde elfo = new ElfoVerde("legolas");
+		Dwarf dwarf = new Dwarf("Gimli");
+		elfo.atirarFlecha(dwarf);
+		assertTrue(elfo.getExperiencia() == 2);
+	}
+
+	@Test
+	public void retornaFalseTentativaAdicionarEspadaDeAcoValirian() {
+		ElfoVerde elfo = new ElfoVerde("legolas");
+		Item item = new Item(1, "Espada de aço valirian");
+		elfo.adicionarItem(item);
+		assertFalse(elfo.getInventario().getItens().contains(item));
+	}
+
+	@Test
+	public void retornaFalseTentaAdicionarItemNull() {
+		ElfoVerde elfo = new ElfoVerde("legolas");
+		Item item = null;
+		elfo.adicionarItem(item);
+		assertFalse(elfo.getInventario().getItens().contains(item));
+	}
 }
-
-
