07ee3452b2d8ef9d17d57da9c63ff04fbce91bc1
Vinicius A <Vinicius A>
2016-04-21 19:24:31 -0300

    test(ItemTest): formatar, melhorar nome variaveis.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ItemTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ItemTest.java
@@ -3,18 +3,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ItemTest{
-    @Test
-    public void cria1ItemEspada(){
-        Item item = new Item(1,"espada");
-        assertEquals(1, item.getQtd());
-        assertTrue(item.getDescricao().equals("espada"));
-    }
-    
-    @Test
-    public void umItemNaoEhIgualAoOutro(){
-        Item item1 = new Item(1, "Espada");
-        Item item2 = new Item(2, "Espada");
-        assertFalse(item1.equals(item2));
-    }
+public class ItemTest {
+	@Test
+	public void cria1ItemEspada() {
+		Item item = new Item(1, "espada");
+		assertEquals(1, item.getQtd());
+		assertTrue(item.getDescricao().equals("espada"));
+	}
+
+	@Test
+	public void umItemNaoEhIgualAoOutro() {
+		Item itemUm = new Item(1, "Espada");
+		Item itemDois = new Item(2, "Espada");
+		assertFalse(itemUm.equals(itemDois));
+	}
 }
