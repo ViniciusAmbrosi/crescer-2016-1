dd96e86040c8d9cf66b5a1b6a195b5eb9251613a
Vinicius A <Vinicius A>
2016-04-21 19:16:32 -0300

    doc(DataTerceiraEra): retirar acentos e retirar alguns comentarios. style(DataTerceiraEra): formatar.

--- src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEra.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEra.java
@@ -1,29 +1,34 @@
 
-public class DataTerceiraEra{
-    private int dia, mes, ano;
+public class DataTerceiraEra {
+	private int dia, mes, ano;
+
+	public DataTerceiraEra(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
     
-    public DataTerceiraEra(int dia, int mes, int ano){ //Validação não definida no exercício, logo(?)
-        this.dia = dia;
-        this.mes = mes;
-        this.ano = ano;
-    }
-    
-    public boolean ehBissexto(){ //é bissexto aquele que é múltiplo de 400 ou é multiplo de 4 mas nao de 100
-        boolean multQuatrocentos = this.ano % 400 == 0; //É multiplo de Quatrocentos?
-        boolean multQuatroNaoMultCem = this.ano % 4 == 0 && this.ano % 100 != 0; //É múltiplo de 4 sem ser de 100?
-        return multQuatrocentos ? true : multQuatroNaoMultCem; //Se é multiplo de quatrocentos, true, se não, é multiplo de 4 sem ser de 100?
+    public boolean ehBissexto() { //Ano bissexto aquele que eh multiplo de 400 ou eh multiplo de 4 mas nao de 100
+        boolean multQuatrocentos = this.ano % 400 == 0; //Eh multiplo de Quatrocentos?
+        boolean multQuatroNaoMultCem = this.ano % 4 == 0 && this.ano % 100 != 0; //Eh multiplo de 4 sem ser de 100?
+        return multQuatrocentos ? true : multQuatroNaoMultCem; //Se eh multiplo de quatrocentos, true, se nao, eh multiplo de 4 sem ser de 100?
     }
 
-    public boolean equals(Object obj){
-        DataTerceiraEra dte = (DataTerceiraEra) obj;
-        return this.dia == dte.dia ?
-               this.mes == dte.mes ? 
-               this.ano == dte.ano ? true : false : false : false;
-    }
-    
-    public int getDia(){return this.dia;}
-    
-    public int getMes(){return this.mes;}
-    
-    public int getAno(){return this.ano;}   
+	public boolean equals(Object obj) {
+		DataTerceiraEra dataTerceiraEra = (DataTerceiraEra) obj;
+		return this.dia == dataTerceiraEra.dia
+				? this.mes == dataTerceiraEra.mes ? this.ano == dataTerceiraEra.ano ? true : false : false : false;
+	}
+
+	public int getDia() {
+		return this.dia;
+	}
+
+	public int getMes() {
+		return this.mes;
+	}
+
+	public int getAno() {
+		return this.ano;
+	}
 }
