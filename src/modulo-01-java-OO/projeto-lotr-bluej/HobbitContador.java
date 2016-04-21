429a148cc34affd111a4d4b6a2b5a3209b6f4bbb
Vinicius A <Vinicius A>
2016-04-21 19:13:57 -0300

    style(HobbitContador): formatar.

--- src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
@@ -1,58 +1,59 @@
 import java.util.*;
 
-public class HobbitContador{
-    public int calcularDiferenca(ArrayList<ArrayList<Integer>> arrayDePares){
-        int produtoPares= 1; 
-        int mmcPares= 0;
-        int produtosMenosMmc = 0;
-        if(arrayDePares == null) 
-            return 0 ;
-        while(arrayDePares.contains(null)) //retira pares nulos
-            arrayDePares.remove(null);
-        for(ArrayList<Integer> pares : arrayDePares){
-            int primeiroNumero = pares.get(0);
-            int segundoNumero = pares.get(1);
-            mmcPares = new Calculador(primeiroNumero, segundoNumero).mmc();
-            produtoPares = primeiroNumero * segundoNumero;
-            produtosMenosMmc += produtoPares - mmcPares;
-        } 
-        return produtosMenosMmc;
-    }
-    
-    public int obterMaiorMultiploDeTresAte(int numero){ 
-        int maior = 0; //retorna maior, logo nao é necessario ArrayList
-        for (int i = numero; i >= 0; i--){ //começa do final
-            if (i % 3 == 0 && i > maior){ //nao verificava se o valor era maior, e alocava todos multiplos de 3 em um arrayList
-                maior = i;
-                break;
-            }
-        }
-        return maior;
-    }
-    
-    public ArrayList<Integer> obterMultiplosDeTresAte(int numero){
-        ArrayList<Integer> multiplos = new ArrayList<>(Arrays.asList(0));
-        for (int i = 3; i <= numero; i += 3){ //otimiza laço com i = 3 e somando mais 3 a i. Variavel utilizada como parametro invalida.
-            if (i % 3 == 0) //break sairia do laço se i % 3 fosse zero
-            multiplos.add(i);
-        }
-        return multiplos;
-    }
-    
-    private class Calculador{
-        private int primeiroNumero, segundoNumero;
-        public Calculador(int primeiroNumero, int segundoNumero){
-            this.primeiroNumero = primeiroNumero;
-            this.segundoNumero = segundoNumero;
-        }
-        
-        private int mmc(){
-            if(segundoNumero == 0)
-                return 0;
-            int aux = primeiroNumero;
-            while(primeiroNumero % segundoNumero != 0) 
-                primeiroNumero += aux;
-                return primeiroNumero;
-        }
-    }       
+public class HobbitContador {
+	public int calcularDiferenca(ArrayList<ArrayList<Integer>> arrayDePares) {
+		int produtoPares = 1;
+		int mmcPares = 0;
+		int produtosMenosMmc = 0;
+		if (arrayDePares == null)
+			return 0;
+		while (arrayDePares.contains(null)) // retira pares nulos
+			arrayDePares.remove(null);
+		for (ArrayList<Integer> pares : arrayDePares) {
+			int primeiroNumero = pares.get(0);
+			int segundoNumero = pares.get(1);
+			mmcPares = new Calculador(primeiroNumero, segundoNumero).mmc();
+			produtoPares = primeiroNumero * segundoNumero;
+			produtosMenosMmc += produtoPares - mmcPares;
+		}
+		return produtosMenosMmc;
+	}
+
+	public int obterMaiorMultiploDeTresAte(int numero) {
+		int maior = 0;
+		for (int i = numero; i >= 0; i--) {
+			if (i % 3 == 0 && i > maior) {
+				maior = i;
+				break;
+			}
+		}
+		return maior;
+	}
+
+	public ArrayList<Integer> obterMultiplosDeTresAte(int numero) {
+		ArrayList<Integer> multiplos = new ArrayList<>(Arrays.asList(0));
+		for (int i = 3; i <= numero; i += 3) {
+			if (i % 3 == 0)
+				multiplos.add(i);
+		}
+		return multiplos;
+	}
+
+	private class Calculador {
+		private int primeiroNumero, segundoNumero;
+
+		public Calculador(int primeiroNumero, int segundoNumero) {
+			this.primeiroNumero = primeiroNumero;
+			this.segundoNumero = segundoNumero;
+		}
+
+		private int mmc() {
+			if (segundoNumero == 0)
+				return 0;
+			int aux = primeiroNumero;
+			while (primeiroNumero % segundoNumero != 0)
+				primeiroNumero += aux;
+			return primeiroNumero;
+		}
+	}
 }
