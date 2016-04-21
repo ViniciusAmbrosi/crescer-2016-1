51664b49d15c9c2564ca6b59213c122d9e1f270f
Vinicius A <Vinicius A>
2016-04-21 19:16:45 -0300

    style(CestoDeLembas): formatar.

--- src/modulo-01-java-OO/projeto-lotr-bluej/CestoDeLembas.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/CestoDeLembas.java
@@ -1,23 +1,12 @@
 
-public class CestoDeLembas{ 
-    private int numeroDePaes;
-    
-    public CestoDeLembas(int numeroDePaes){
-        this.numeroDePaes = numeroDePaes;
-    }
-    
-    public boolean podeDividirEmPares(){
-        return numeroDePaes > 2 && numeroDePaes <= 100 && numeroDePaes % 2 == 0;
-    }
+public class CestoDeLembas {
+	private int numeroDePaes;
+
+	public CestoDeLembas(int numeroDePaes) {
+		this.numeroDePaes = numeroDePaes;
+	}
+
+	public boolean podeDividirEmPares() {
+		return numeroDePaes > 2 && numeroDePaes <= 100 && numeroDePaes % 2 == 0;
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
