ed23b01244335872b8163dd94d24a02098590a6f
Vinicius A <Vinicius A>
2016-04-21 19:26:57 -0300

    style(ElfoTest): formatar.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoTest.java
@@ -3,89 +3,87 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ElfoTest{
-    @Before
-    public void tearDown() {
-        System.gc();
-        System.runFinalization();
-    }
-    
-    @Test
-    public void criaElfoTesteNome(){
-        Elfo elfo = new Elfo("nome");
-        assertEquals("nome", elfo.getNome());
-    }
-    
-    @Test
-    public void criaElfoTesteFlecha(){
-        Elfo elfo = new Elfo("nome");
-        assertEquals(42, elfo.getFlechas());
-    }
-    
-    @Test
-    public void criaElfoTesteExp(){
-        Elfo elfo = new Elfo("nome");
-        assertEquals(0, elfo.getExperiencia());
-    }
+public class ElfoTest {
+	@Before
+	public void tearDown() {
+		System.gc();
+		System.runFinalization();
+	}
 
-    @Test
-    public void atiraFlechaAnaoTesteXp(){
-        Elfo elfo = new Elfo("nome");
-        Dwarf dwarf = new Dwarf("nome");
-        elfo.atirarFlecha(dwarf);
-        assertEquals(1, elfo.getExperiencia());
-    }
-        
-    @Test
-    public void atiraFlechaAnaoTesteFlecha(){
-        Elfo elfo = new Elfo("nome");
-        Dwarf dwarf = new Dwarf("nome");
-        elfo.atirarFlecha(dwarf);
-        assertEquals(41, elfo.getFlechas());
-    }
-       
-    @Test
-    public void atiraFlechaAnaoTesteVidaAnao(){
-        Elfo elfo = new Elfo("nome");
-        Dwarf dwarf = new Dwarf("nome");
-        elfo.atirarFlecha(dwarf);
-        assertTrue(dwarf.getVida() == 100);
-    }
-    
-    @Test
-    public void elfoTesteToString(){
-        Elfo elfo = new Elfo("nome");
-        assertEquals("nome possui 42 flechas e 0 níveis de experiência", elfo.toString());
-    }
-    
-    @Test
-    public void elfoSegundoConstrutor(){
-        Elfo elfo = new Elfo("nome", 45);
-        assertEquals("nome", elfo.getNome());
-        assertEquals(45, elfo.getFlechas());
-        assertEquals(0, elfo.getExperiencia());
-    }
-    
-    @Test
-    public void retorna100VidaIrishDwarf(){
-        Elfo elfo = new Elfo("legolas");
-        Dwarf dwarf = new IrishDwarf("nome");
-        elfo.atirarFlecha(dwarf);
-        assertTrue(dwarf.getVida() == 100);
-    }
-    
-    @Test
-    public void conta10Elfos(){
-        for(int i = 0; i < 10; i++){
-            Elfo elfo = new Elfo("elfo" + i);
-        }
-        assertEquals(10, Elfo.countElfo);
-    }
-    
-    @Test
-    public void conta0Elfos(){
-        assertEquals(0, Elfo.countElfo);
-    }
+	@Test
+	public void criaElfoTesteNome() {
+		Elfo elfo = new Elfo("nome");
+		assertEquals("nome", elfo.getNome());
+	}
+
+	@Test
+	public void criaElfoTesteFlecha() {
+		Elfo elfo = new Elfo("nome");
+		assertEquals(42, elfo.getFlechas());
+	}
+
+	@Test
+	public void criaElfoTesteExp() {
+		Elfo elfo = new Elfo("nome");
+		assertEquals(0, elfo.getExperiencia());
+	}
+
+	@Test
+	public void atiraFlechaAnaoTesteXp() {
+		Elfo elfo = new Elfo("nome");
+		Dwarf dwarf = new Dwarf("nome");
+		elfo.atirarFlecha(dwarf);
+		assertEquals(1, elfo.getExperiencia());
+	}
+
+	@Test
+	public void atiraFlechaAnaoTesteFlecha() {
+		Elfo elfo = new Elfo("nome");
+		Dwarf dwarf = new Dwarf("nome");
+		elfo.atirarFlecha(dwarf);
+		assertEquals(41, elfo.getFlechas());
+	}
+
+	@Test
+	public void atiraFlechaAnaoTesteVidaAnao() {
+		Elfo elfo = new Elfo("nome");
+		Dwarf dwarf = new Dwarf("nome");
+		elfo.atirarFlecha(dwarf);
+		assertTrue(dwarf.getVida() == 100);
+	}
+
+	@Test
+	public void elfoTesteToString() {
+		Elfo elfo = new Elfo("nome");
+		assertEquals("nome possui 42 flechas e 0 níveis de experiência", elfo.toString());
+	}
+
+	@Test
+	public void elfoSegundoConstrutor() {
+		Elfo elfo = new Elfo("nome", 45);
+		assertEquals("nome", elfo.getNome());
+		assertEquals(45, elfo.getFlechas());
+		assertEquals(0, elfo.getExperiencia());
+	}
+
+	@Test
+	public void retorna100VidaIrishDwarf() {
+		Elfo elfo = new Elfo("legolas");
+		Dwarf dwarf = new IrishDwarf("nome");
+		elfo.atirarFlecha(dwarf);
+		assertTrue(dwarf.getVida() == 100);
+	}
+
+	@Test
+	public void conta10Elfos() {
+		for (int i = 0; i < 10; i++) {
+			Elfo elfo = new Elfo("elfo" + i);
+		}
+		assertEquals(10, Elfo.getCountElfos());
+	}
+
+	@Test
+	public void conta0Elfos() {
+		assertEquals(0, Elfo.getCountElfos());
+	}
 }
-
-
