79668e726da9ce85dc3526f218681da054d85c6a
Vinicius A <Vinicius A>
2016-04-21 19:22:40 -0300

    test(DataTerceiraEraTest): formatar, melhorar nome variaveis.

--- src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEraTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEraTest.java
@@ -3,41 +3,41 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class DataTerceiraEraTest{   
-    public void criaDataTerceiraEraAno(){
-        DataTerceiraEra dte = new DataTerceiraEra(15,12,1000);
-        assertEquals(15, dte.getDia());
-        assertEquals(12, dte.getMes());
-        assertEquals(1000, dte.getAno());
-    }
-    
-    @Test
-    public void ano2100NaoEhBissexto(){
-        DataTerceiraEra dte = new DataTerceiraEra(15,12,2100);
-        assertEquals(false, dte.ehBissexto());
-    }
-    
-    @Test
-    public void ano4000EhBissexto(){
-        DataTerceiraEra dte = new DataTerceiraEra(15,12,4000);
-        assertEquals(true, dte.ehBissexto());
-    }
-    
-    @Test
-    public void ano90000NaoEhBissexto(){
-        DataTerceiraEra dte = new DataTerceiraEra(15,12,9000);
-        assertEquals(false, dte.ehBissexto());
-    }
-    
-    @Test
-    public void ano2000EhBissexto(){
-        DataTerceiraEra dte = new DataTerceiraEra(15,12,2000);
-        assertEquals(true, dte.ehBissexto());
-    }
-    
-    @Test
-    public void ano1500NaoEhBissexto(){
-        DataTerceiraEra dte = new DataTerceiraEra(15,12,1500);
-        assertEquals(false, dte.ehBissexto());
-    }    
+public class DataTerceiraEraTest {
+	public void criaDataTerceiraEraAno() {
+		DataTerceiraEra dataTerceiraEra = new DataTerceiraEra(15, 12, 1000);
+		assertEquals(15, dataTerceiraEra.getDia());
+		assertEquals(12, dataTerceiraEra.getMes());
+		assertEquals(1000, dataTerceiraEra.getAno());
+	}
+
+	@Test
+	public void ano2100NaoEhBissexto() {
+		DataTerceiraEra dataTerceiraEra = new DataTerceiraEra(15, 12, 2100);
+		assertEquals(false, dataTerceiraEra.ehBissexto());
+	}
+
+	@Test
+	public void ano4000EhBissexto() {
+		DataTerceiraEra dataTerceiraEra = new DataTerceiraEra(15, 12, 4000);
+		assertEquals(true, dataTerceiraEra.ehBissexto());
+	}
+
+	@Test
+	public void ano90000NaoEhBissexto() {
+		DataTerceiraEra dataTerceiraEra = new DataTerceiraEra(15, 12, 9000);
+		assertEquals(false, dataTerceiraEra.ehBissexto());
+	}
+
+	@Test
+	public void ano2000EhBissexto() {
+		DataTerceiraEra dataTerceiraEra = new DataTerceiraEra(15, 12, 2000);
+		assertEquals(true, dataTerceiraEra.ehBissexto());
+	}
+
+	@Test
+	public void ano1500NaoEhBissexto() {
+		DataTerceiraEra dataTerceiraEra = new DataTerceiraEra(15, 12, 1500);
+		assertEquals(false, dataTerceiraEra.ehBissexto());
+	}
 }
