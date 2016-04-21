572bb50e680a34f4d7487fd2ce9aaec184beff28
Vinicius A <Vinicius A>
2016-04-21 19:15:35 -0300

    style(NoturnosPorUltimo): melhorar nome de variaveis e formatar.

--- src/modulo-01-java-OO/projeto-lotr-bluej/NoturnosPorUltimo.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/NoturnosPorUltimo.java
@@ -1,41 +1,41 @@
 import java.util.*;
 
 public class NoturnosPorUltimo implements Estrategia {
-    ArrayList<Elfo> ordemAtaque = new ArrayList<Elfo>();
+	ArrayList<Elfo> ordemAtaque = new ArrayList<Elfo>();
 
-    public void atacar(HashMap<Status, ArrayList<Elfo>> exercitoElfos, ArrayList<Dwarf> dwarfs) {
-    	ArrayList<Elfo> exercitoElfosVivos = exercitoElfos.get(Status.VIVO);
-        if (dwarfs == null || exercitoElfosVivos == null  || dwarfs.isEmpty())
-            return;
-        Class current = ElfoVerde.class;
-        Iterator<Elfo> it = exercitoElfosVivos.iterator();
-        while(it.hasNext()){
-            Elfo elfo = it.next();
-            if(elfo.getClass().equals(current)){
-                ordemAtaqueElfos(elfo.getClass(), exercitoElfosVivos, dwarfs);
-                if(current.equals(ElfoNoturno.class))
-                    break;
-                current = ElfoNoturno.class;
-                it = exercitoElfosVivos.iterator();
-            }
-            if(!it.hasNext() && current.equals(ElfoVerde.class)){
-                it = exercitoElfosVivos.iterator();
-                current = ElfoNoturno.class;
-            }
-        }
-    }
-
-    private void ordemAtaqueElfos(Class tipo, ArrayList<Elfo> exercitoElfos, ArrayList<Dwarf> dwarfs) {
-        for (Elfo elfo : exercitoElfos) {
-            if (elfo.getClass().equals(tipo)) {
-                ordemAtaque.add(elfo);
-                for (Dwarf dwarf : dwarfs)
-                    elfo.atirarFlecha(dwarf);
-            }
+	public void atacar(HashMap<Status, ArrayList<Elfo>> exercitoElfos, ArrayList<Dwarf> dwarfs) {
+		ArrayList<Elfo> exercitoElfosVivos = exercitoElfos.get(Status.VIVO);
+		if (dwarfs == null || exercitoElfosVivos == null || dwarfs.isEmpty())
+			return;
+		Class current = ElfoVerde.class;
+		Iterator<Elfo> iterator = exercitoElfosVivos.iterator();
+		while (iterator.hasNext()) {
+			Elfo elfo = iterator.next();
+			if (elfo.getClass().equals(current)) {
+				ordemAtaqueElfos(elfo.getClass(), exercitoElfosVivos, dwarfs);
+				if (current.equals(ElfoNoturno.class))
+					break;
+				current = ElfoNoturno.class;
+				iterator = exercitoElfosVivos.iterator();
+			}
+			if (!iterator.hasNext() && current.equals(ElfoVerde.class)) {
+				iterator = exercitoElfosVivos.iterator();
+				current = ElfoNoturno.class;
+			}
 		}
 	}
-	
-	public ArrayList<Elfo> getOrdemDoUltimoAtaque(){
-       return ordemAtaque;
-    }
+
+	private void ordemAtaqueElfos(Class tipo, ArrayList<Elfo> exercitoElfos, ArrayList<Dwarf> dwarfs) {
+		for (Elfo elfo : exercitoElfos) {
+			if (elfo.getClass().equals(tipo)) {
+				ordemAtaque.add(elfo);
+				for (Dwarf dwarf : dwarfs)
+					elfo.atirarFlecha(dwarf);
+			}
+		}
+	}
+
+	public ArrayList<Elfo> getOrdemDoUltimoAtaque() {
+		return ordemAtaque;
+	}
 }
