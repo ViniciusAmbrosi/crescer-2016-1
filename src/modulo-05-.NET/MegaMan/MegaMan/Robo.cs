fc366c4d8f8eea9c194b6c3204045d537fe8537e
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-05-06 18:21:07 -0300

    feat(megaman): adicionar parte da resolução do exercicio de megaman.

---
+++ src/modulo-05-.NET/MegaMan/MegaMan/Robo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaMan
+{
+    public abstract class Robo
+    {
+        public abstract string Nome { get; }
+        public int Vida { get; private set; }
+        protected virtual int Ataque {
+            get
+            {
+                return 5;
+            }
+        }
+        protected virtual int Defesa
+        {
+            get
+            {
+                return 0;
+            }
+        }
+        public void Atacar(Robo robo) {
+            robo.Vida -= this.Ataque - robo.Defesa;
+        }
+    }
+}
