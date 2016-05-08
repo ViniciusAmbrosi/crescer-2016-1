19210beef5cd109edb07a76f6a64694b3ed76256
Vinicius A <Vinicius A>
2016-05-08 16:38:10 -0300

    feat(CanhaoDePlasma): adicionar classe da interface canhao de plasma.

---
+++ src/modulo-05-.NET/JogoMegaMan/JogoMegaMan/CanhaoDePlasma.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoMegaMan
+{
+    public class CanhaoDePlasma : IUpgrade
+    {
+        public int BonusAtaque
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        public int BonusDefesa
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public string TipoUpgrade
+        {
+            get
+            {
+                return "UpgradeDeAtaque";
+            }
+        }
+    }
+}
