ca10a6500082221657c2f3f849fe1e0ed8fc2291
Vinicius A <Vinicius A>
2016-05-08 16:35:33 -0300

    feat(Megaman): adicionar classe completa do robo megaman.

---
+++ src/modulo-05-.NET/JogoMegaMan/JogoMegaMan/MegaMan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoMegaMan
+{
+    public class MegaMan : Robo
+    {
+        public override string Nome
+        {
+            get
+            {
+                return "Megaman";
+            }
+        }
+        protected override int Ataque
+        {
+            get
+            {
+                return 6 + BonusEquipAtaque;
+            }
+        }
+
+        public virtual void Atacar(Robo robo)
+        {
+            if (Vida <= 30)
+                robo.RecebeDano(3 + Ataque);
+            else
+                robo.RecebeDano(Ataque);
+        }
+    }
+}
