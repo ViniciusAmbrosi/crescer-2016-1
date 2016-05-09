39db77efd7d551f3605c7e733acdc027a775847f
Vinicius A <Vinicius A>
2016-05-09 00:02:57 -0300

    test(ProtoManTest): adicionar testes para chip.

--- src/modulo-05-.NET/JogoMegaMan/JogoMegaManTest/ProtoManTest.cs
+++ src/modulo-05-.NET/JogoMegaMan/JogoMegaManTest/ProtoManTest.cs
@@ -149,11 +149,11 @@ namespace JogoMegaManTest
             {
                 var protoman = new ProtoMan();
                 var rush = new Rush();
-                var botDois = new Bot();
+                var bot = new Bot();
                 protoman.EquiparUpgrade(rush);
-                protoman.Atacar(botDois);
-                Assert.AreEqual(botDois.Vida, 91);
-                botDois.Atacar(protoman);
+                protoman.Atacar(bot);
+                Assert.AreEqual(bot.Vida, 91);
+                bot.Atacar(protoman);
                 Assert.AreEqual(protoman.Vida, 100);
             }
 
@@ -178,12 +178,14 @@ namespace JogoMegaManTest
             {
                 var protoman = new ProtoMan();
                 var rush = new Rush();
-                var botDois = new Bot();
+                var bot = new Bot();
                 var escudo = new EscudoDeEnergia();
                 rush.EquiparUpgrade(escudo);
                 rush.EquiparUpgrade(escudo);
                 protoman.EquiparUpgrade(rush);
-                Assert.AreEqual(protoman.toString(), "Nome: {Protoman}, Vida: {100}, Ataque: {9}, Defesa: {5}");
+                protoman.Atacar(bot);
+                Assert.AreEqual(bot.Vida, 91);
+                Assert.AreEqual(protoman.toString(), "Nome: {Protoman}, Vida: {100}, Ataque: {5}, Defesa: {2}");
             }
 
             [TestMethod]
@@ -204,18 +206,22 @@ namespace JogoMegaManTest
             }
 
             [TestMethod]
-            public void MegaManEquipaEscudoCanhaoERushEquipadoDoisCanhoes()
+            public void ProtoManEquipaEscudoCanhaoERushEquipadoDoisCanhoes()
             {
                 var protoman = new ProtoMan();
                 var rush = new Rush();
                 var canhao = new CanhaoDePlasma();
                 var escudo = new EscudoDeEnergia();
+                var bot = new Bot();
                 rush.EquiparUpgrade(canhao);
                 rush.EquiparUpgrade(canhao);
                 protoman.EquiparUpgrade(rush);
                 protoman.EquiparUpgrade(canhao);
                 protoman.EquiparUpgrade(escudo);
-                Assert.AreEqual(protoman.toString(), "Nome: {Protoman}, Vida: {100}, Ataque: {15}, Defesa: {7}");
+                protoman.Atacar(bot);
+                bot.Atacar(protoman);
+                Assert.AreEqual(bot.Vida, 85);
+                Assert.AreEqual(protoman.Vida, 100);
             }
 
             [TestMethod]
@@ -224,12 +230,115 @@ namespace JogoMegaManTest
                 var protoman = new ProtoMan();
                 var rush = new Rush();
                 var rushEquipDeRush = new Rush();
+                var bot = new Bot();
                 rush.EquiparUpgrade(rushEquipDeRush);
                 rush.EquiparUpgrade(rushEquipDeRush);
                 protoman.EquiparUpgrade(rush);
                 protoman.EquiparUpgrade(rush);
                 protoman.EquiparUpgrade(rush);
-                Assert.AreEqual(protoman.toString(), "Nome: {Protoman}, Vida: {100}, Ataque: {41}, Defesa: {11}");
+                protoman.Atacar(bot);
+                Assert.AreEqual(bot.Vida, 59);
+            }
+
+            [TestMethod]
+            public void ProtoManChipPadrao()
+            {
+                var protoman = new ProtoMan();
+                Assert.AreEqual(protoman.toString(), "Nome: {Protoman}, Vida: {100}, Ataque: {5}, Defesa: {2}");
+            }
+
+            [TestMethod]
+            public void ProtoManComChip1()
+            {
+                var protoman = new ProtoMan(Chip.Nivel1);
+                Assert.AreEqual(protoman.toString(), "Nome: {Protoman}, Vida: {100}, Ataque: {4}, Defesa: {2}");
+            }
+
+            [TestMethod]
+            public void ProtoManComChip1AtacaChip1()
+            {
+                var protoman = new ProtoMan(Chip.Nivel1);
+                var bot = new Bot(Chip.Nivel1);
+                protoman.Atacar(bot);
+                Assert.AreEqual(bot.Vida, 96);
+
+            }
+
+            [TestMethod]
+            public void ProtoManComChip1AtacaChip3()
+            {
+                var protoman = new ProtoMan(Chip.Nivel1);
+                var bot = new Bot(Chip.Nivel3);
+                protoman.Atacar(bot);
+                Assert.AreEqual(bot.Vida, 97);
+
+            }
+
+            [TestMethod]
+            public void ProtoManComChip2()
+            {
+                var protoman = new ProtoMan(Chip.Nivel2);
+                Assert.AreEqual(protoman.toString(), "Nome: {Protoman}, Vida: {100}, Ataque: {5}, Defesa: {2}");
+            }
+
+            [TestMethod]
+            public void ProtoManComChip2AtacaChip1()
+            {
+                var protoman = new ProtoMan(Chip.Nivel2);
+                var bot = new Bot(Chip.Nivel1);
+                protoman.Atacar(bot);
+                Assert.AreEqual(bot.Vida, 95);
+
+            }
+
+            [TestMethod]
+            public void ProtoManComChip2AtacaChip3()
+            {
+                var protoman = new ProtoMan(Chip.Nivel2);
+                var bot = new Bot(Chip.Nivel3);
+                protoman.Atacar(bot);
+                Assert.AreEqual(bot.Vida, 96);
+
+            }
+
+            [TestMethod]
+            public void ProtoManComChip3()
+            {
+                var protoman = new ProtoMan(Chip.Nivel3);
+                Assert.AreEqual(protoman.toString(), "Nome: {Protoman}, Vida: {100}, Ataque: {7}, Defesa: {3}");
+
+            }
+
+            [TestMethod]
+            public void ProtoManComChip3AtacaChip1()
+            {
+                var protoman = new ProtoMan(Chip.Nivel3);
+                var bot = new Bot(Chip.Nivel1);
+                protoman.Atacar(bot);
+                Assert.AreEqual(bot.Vida, 93);
+
+            }
+
+            [TestMethod]
+            public void ProtoManComChip3AtacaChip3()
+            {
+                var protoman = new ProtoMan(Chip.Nivel3);
+                var bot = new Bot(Chip.Nivel3);
+                protoman.Atacar(bot);
+                Assert.AreEqual(bot.Vida, 94);
+            }
+
+            [TestMethod]
+            public void ProtoManComChip3EReviveuVidaCausa8Dano()
+            {
+                var protoman = new ProtoMan(Chip.Nivel3);
+                var bot = new Bot(Chip.Nivel3);
+                for (int i = 0; i < 40; i++)
+                {
+                    bot.Atacar(protoman);
+                }
+                protoman.Atacar(bot);
+                Assert.AreEqual(bot.Vida, 92);
             }
         }
     }
