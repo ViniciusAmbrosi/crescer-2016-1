db300eb22e19f16daf3d2c81c426085b12da84d7
Vinicius A <Vinicius A>
2016-05-10 21:42:29 -0300

    feat(SobreMimModel): adicionar Model de apoio para SobreMim.

---
+++ src/modulo-05-.NET/MVC-StreetFighter/MVC-StreetFighter/Models/StreetFighter/SobreMimModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_StreetFighter.Models.StreetFighter
+{
+    public class SobreMimModel
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+        public List<String> Hobbies { get; set; }
+        public string SrcFoto { get; set; }
+
+    }
+}
