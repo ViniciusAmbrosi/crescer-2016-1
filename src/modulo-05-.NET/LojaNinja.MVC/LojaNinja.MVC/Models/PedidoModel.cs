feb3487d81e17a00190a71f23967458c64cb6886
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-05-12 18:55:09 -0300

    feat(PedidoModel): adicionar classe PedidoModel do front.

---
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.MVC/Models/PedidoModel.cs
@@ -0,0 +1,38 @@
+﻿using LojaNinja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinja.MVC.Models
+{
+    public class PedidoModel
+    {
+        [Required(ErrorMessage = "Deve informar data de entrega")]
+        [DisplayName("Data de Entrega")]
+        public DateTime DataEntrega { get; set; }
+
+        [Required(ErrorMessage = "Deve ser informado o nome do produto")]
+        [DisplayName("Nome do Produto")]
+        public string NomeProduto { get; set; }
+
+        [Required(ErrorMessage = "Deve ser informado o valor")]
+        public decimal Valor { get; set; }
+
+        [Required(ErrorMessage = "Deve ser informado o tipo de pagamento")]
+        [DisplayName("Tipo de Pagamento")]
+        public TipoPagamento TipoDePagamento { get; set; }
+
+        [Required(ErrorMessage = "Deve ser informado o nome do cliente")]
+        [DisplayName("Nome do Cliente")]
+        public string NomeCliente { get; set; }
+
+        [Required(ErrorMessage = "Deve ser informado a cidade")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Deve ser informado o estado")]
+        public string Estado { get; set; }
+    }
+}
