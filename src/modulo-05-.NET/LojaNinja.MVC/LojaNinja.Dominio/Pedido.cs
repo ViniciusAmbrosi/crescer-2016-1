a978e494943592b4d900b82dbfa6b96067bc74ad
Vinicius A <Vinicius A>
2016-05-13 10:03:51 -0300

    feat(Pedido): adicionar construtores completos e métodos auxiliares.

--- src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.Dominio/Pedido.cs
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.Dominio/Pedido.cs
@@ -18,6 +18,25 @@ namespace LojaNinja.Dominio
             Cidade = cidade;
             Estado = estado;
             DataPedido = DateTime.Now;
+
+            var diasRestantesParaConcluirEntrega = (DataEntrega - DataPedido).TotalDays;
+            ValidaPossibilidadeEntrega(diasRestantesParaConcluirEntrega);
+            DefineUrgenciaDoPedido(diasRestantesParaConcluirEntrega);
+        }
+
+        public Pedido(int numeroPedido, DateTime dataPedido, DateTime dataEntrega, string nomeProduto, decimal valor, TipoPagamento tipoDePagamento, string nomeCliente, string cidade, string estado, bool urgente)
+        {
+            NumeroPedido = numeroPedido;
+            DataPedido = dataPedido;
+            DataEntrega = dataEntrega;
+            NomeProduto = nomeProduto;
+            Valor = valor;
+            TipoDePagamento = tipoDePagamento;
+            NomeCliente = nomeCliente;
+            Cidade = cidade;
+            Estado = estado;
+            DataPedido = DateTime.Now;
+            PedidoUrgente = urgente;
         }
 
         public DateTime DataEntrega { get; private set; }
@@ -28,6 +47,27 @@ namespace LojaNinja.Dominio
         public string NomeCliente { get; private set; }
         public string Cidade { get; private set; }
         public string Estado { get; private set; }
-        public int NumeroPedido { get; private set; }
+        public int? NumeroPedido { get; private set; }
+        public bool PedidoUrgente { get; private set; }
+
+
+        private void ValidaPossibilidadeEntrega(double diasRestantesParaConcluirEntrega)
+        {
+            if (diasRestantesParaConcluirEntrega < 1)
+                throw new ArgumentException("A data de entrega deve ser no mínimo 1 dia maior do que a data atual.");
+        }
+
+        private void DefineUrgenciaDoPedido(double diasRestantesParaConcluirEntrega)
+        {
+            PedidoUrgente = diasRestantesParaConcluirEntrega < 7;
+        }
+
+        public void AtualizarId(int id)
+        {
+            if (NumeroPedido.HasValue)
+                throw new InvalidOperationException("Esse objeto já possuia Id, portanto ele já havia sido salvo no banco. Não é possível alterar esse valor.");
+            NumeroPedido = id;
+        }
     }
+
 }
