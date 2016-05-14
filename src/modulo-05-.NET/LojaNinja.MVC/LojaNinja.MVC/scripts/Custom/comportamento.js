cd21c99e2bdfb0330b2af7c4f2a511375b8733d1
Vinicius A <Vinicius A>
2016-05-14 00:34:20 -0300

    feat(projeto): adicionar feature de click.

--- src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.MVC/scripts/Custom/comportamento.js
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.MVC/scripts/Custom/comportamento.js
@@ -2,5 +2,11 @@
 
 $(function () {
     $("#datepicker").datepicker({ dateFormat: "dd/mm/yy" });
+
+    $("#camelao").click(function () {
+        //$("#camelao-div-bottom").css("left", "2000px")
+        $("#camelao-div-bottom").animate({"left": "+=2500px"}, "slow" )
+        //$(".block").animate({ "left": "+=50px" }, "slow");
+});
 });
 
