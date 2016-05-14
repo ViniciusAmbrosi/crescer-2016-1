d7a54e77a668b0987a1b7f2a1388e092e312687a
Vinicius A <Vinicius A>
2016-05-14 01:17:46 -0300

    feat(comportamento): finalizar comportamento de animação.

--- src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.MVC/scripts/Custom/comportamento.js
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.MVC/scripts/Custom/comportamento.js
@@ -2,11 +2,14 @@
 
 $(function () {
     $("#datepicker").datepicker({ dateFormat: "dd/mm/yy" });
-
     $("#camelao").click(function () {
-        //$("#camelao-div-bottom").css("left", "2000px")
-        $("#camelao-div-bottom").animate({"left": "+=2500px"}, "slow" )
+        if ($("#camelao-div-bottom").position().left == -301) {
+            $("#camelao-div-bottom").animate({ "left": "+=2500px" },"slow", function () { $("#camelao-div-bottom").css("display", "none") } )
+        }
+        else {
+            $("#camelao-div-bottom").css("display", "block")
+            $("#camelao-div-bottom").animate({ "left": "-=2500px" }, "slow")
+        };
         //$(".block").animate({ "left": "+=50px" }, "slow");
+    });
 });
-});
-
