eb3d0e042669fdccd8337215dd903bc28a1aedba
Vinicius A <Vinicius A>
2016-05-15 20:52:28 -0300

    feat(UsuarioLogadoModel): adicionar model para usuario logado.

---
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.MVC/Models/UsuarioLogadoModel.cs
@@ -0,0 +1,30 @@
+﻿using LojaNinja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinja.MVC.Models
+{
+    public class UsuarioLogadoModel
+    {
+        public UsuarioLogadoModel(Usuario usuario)
+        {
+            Nome = usuario.PrimeiroNome;
+            Email = usuario.Email;
+            Permissoes = usuario.Permissoes;
+            ID = usuario.ID;
+        }
+
+        public string Email { get; private set; }
+        public string Nome { get; private set; }
+        public string[] Permissoes { get; private set;}
+        public int ID { get; private set; }
+
+        public bool TemPermissao(string permissao)
+        {
+            return Permissoes != null
+                   && Permissoes.Any(p => p.Equals(permissao));
+        }
+    }
+}
