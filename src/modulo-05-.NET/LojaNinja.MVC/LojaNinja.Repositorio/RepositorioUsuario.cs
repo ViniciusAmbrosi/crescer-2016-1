7d07a7ba48db12a7c40b6a40766d37117782ae25
Vinicius A <Vinicius A>
2016-05-15 20:50:40 -0300

    feat(RepositorioUsuario): adicionar repositorio de usuario.

---
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.Repositorio/RepositorioUsuario.cs
@@ -0,0 +1,30 @@
+﻿using LojaNinja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNinja.Repositorio
+{
+    public class RepositorioUsuario : IUsuarioRepositorio
+    {
+        static List<Usuario> Usuarios = new List<Usuario>();
+
+        public void InserirUsuario(Usuario user)
+        {
+            user.AdicionarId(Usuarios.Count + 1);
+            Usuarios.Add(user);
+        }
+
+        public Usuario BuscarUsuarioPorAutenticacao(string email, string senha)
+        {
+            return Usuarios.FirstOrDefault(user => user.Email.Equals(email) && senha.Equals(senha));
+        }
+
+        public Usuario ObterPorId(int id)
+        {
+            return Usuarios[id - 1];
+        }
+    }
+}
