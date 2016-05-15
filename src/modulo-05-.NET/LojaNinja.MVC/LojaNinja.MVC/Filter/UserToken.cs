651e85c6c0ffeeac9627698ac23bbd80879240a1
Vinicius A <Vinicius A>
2016-05-15 20:53:05 -0300

    feat(UserToken): adicionar controlador de autenticação.

---
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.MVC/Filter/UserToken.cs
@@ -0,0 +1,66 @@
+﻿using LojaNinja.MVC.Models;
+using LojaNinja.MVC.Services;
+using LojaNinja.MVC.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+
+namespace LojaNinja.MVC.Filter
+{
+    public class UserToken : AuthorizeAttribute
+    {
+        private string[] _permissoesRequeridas = null;
+
+        public UserToken()
+        {
+            var roles = this.Roles;
+            _permissoesRequeridas = String.IsNullOrWhiteSpace(roles) ? null : roles.Split(',');
+        }
+
+        private bool TemAutorizacao
+        {
+            get
+            {
+                UsuarioLogadoModel usuarioLogado = ServicoDeSessao.UsuarioLogado;
+                if (this._permissoesRequeridas != null)
+                {
+                    foreach (string permissao in _permissoesRequeridas)
+                    {
+                        if (usuarioLogado.TemPermissao(permissao))
+                        {
+                            return true;
+                        }
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            return ServicoDeSessao.EstaLogado && TemAutorizacao;
+        }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            bool estaAutenticadoEAutorizado = this.AuthorizeCore(filterContext.HttpContext);
+            if (!estaAutenticadoEAutorizado)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                                   new RouteValueDictionary
+                                   {
+                                       { "action", "Index" },
+                                       { "controller", "Usuario" }
+                                   });
+            }
+        }
+    }
+}
