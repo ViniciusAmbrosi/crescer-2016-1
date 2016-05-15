4744cde20b1220f047a28aa65f7f748de8081835
Vinicius A <Vinicius A>
2016-05-15 20:53:46 -0300

    feat(UsuarioServico): adicionar classe de apoio para armazenar usuarios.

---
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.Dominio/UsuarioServico.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNinja.Dominio
+{
+    public class UsuarioServico
+    {
+        private IUsuarioRepositorio repoUsuarios;
+
+        public UsuarioServico(IUsuarioRepositorio usuarioRepositorio)
+        {
+            repoUsuarios = usuarioRepositorio;
+        }
+
+        public string Criptografar(string texto)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(texto));
+                StringBuilder sBuilder = new StringBuilder();
+
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                return sBuilder.ToString();
+            }
+        }
+
+        public Usuario BuscarUsuarioPorAutenticacao(string email, string senha)
+        {
+            var senhaCripto = Criptografar(senha);
+            return repoUsuarios.BuscarUsuarioPorAutenticacao(email, senhaCripto);
+        }
+    }
+}
