0da15bd53c10fae6b3204a1903b6c09c5cd91b94
Vinicius A <Vinicius A>
2016-05-15 20:52:38 -0300

    feat(LoginModel): adicionar model para login.

---
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.MVC/Models/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinja.MVC.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [DisplayName("Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DisplayName("Senha")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Senhas possuem de 6 a 20 caracteres")]
+        public string Senha { get; set; }
+    }
+}
