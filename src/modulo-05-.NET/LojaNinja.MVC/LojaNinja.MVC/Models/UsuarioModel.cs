30bc49751802d13caff14871c18bce850f9d7d2d
Vinicius A <Vinicius A>
2016-05-16 00:10:38 -0300

    refactor(UsuarioModel): remover propriedades desnecessárias.

--- src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.MVC/Models/UsuarioModel.cs
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.MVC/Models/UsuarioModel.cs
@@ -9,33 +9,28 @@ namespace LojaNinja.MVC.Models
 {
     public class UsuarioModel
     {
-        [Required(ErrorMessage = "Você deve informar o primeiro nome.")]
-        [DisplayName("Primeiro Nome")]
-        public string PrimeiroNome { get; set; }
+        [Required(ErrorMessage = "Você deve informar um nome.")]
+        [DisplayName("Nome")]
+        public string Nome { get; set; }
 
-        [Required(ErrorMessage = "Você deve informar o ultimo nome")]
-        [DisplayName("Último Nome")]
-        public string UltimoNome { get; set; }
-
-        [Required(ErrorMessage = "Você deve informar o cpf")]
-        [MaxLength(11, ErrorMessage = "CPF deve ter no máximo 11 digitos")]
-        [MinLength(11, ErrorMessage = "CPF deve ter no minimo 11 digitos")]
-        [RegularExpression("^[0-9]*$", ErrorMessage = "CPF deve ser numérico")]
-        [DisplayName("CPF")]
-        public string CPF { get; set; }
-
-        [Required(ErrorMessage = "Você deve informar sua data de nascimento")]
-        [DisplayName("Data de nascimento")]
-        public DateTime DataNascimento { get; set; }
+        //[Required(ErrorMessage = "Você deve informar o cpf")]
+        //[MaxLength(11, ErrorMessage = "CPF deve ter no máximo 11 digitos")]
+        //[MinLength(11, ErrorMessage = "CPF deve ter no minimo 11 digitos")]
+        //[RegularExpression("^[0-9]*$", ErrorMessage = "CPF deve ser numérico")]
+        //[DisplayName("CPF")]
+        //public string CPF { get; set; }
 
         [Required(ErrorMessage = "Você deve informar seu email")]
         [DisplayName("Email")]
         [EmailAddress]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Você deve informar seu email")]
-        [StringLength(20, MinimumLength = 6, ErrorMessage = "A senha deve conter de 6 a 20 caracteres")]
+        [Required(ErrorMessage = "Você deve informar sua senha")]
         [DisplayName("Senha")]
         public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Você deve informar uma confirmação para a senha")]
+        [DisplayName("Confirmar senha")]
+        public string ConfirmaSenha { get; set; }
     }
 }
