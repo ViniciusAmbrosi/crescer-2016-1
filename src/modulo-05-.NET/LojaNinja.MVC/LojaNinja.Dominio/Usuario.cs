4616df809771fba073e375d6e99487e9a1aad504
Vinicius A <Vinicius A>
2016-05-16 00:06:40 -0300

    refactor(Usuario): remover propriedades desnecessárias de acordo com o exercicio.

--- src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.Dominio/Usuario.cs
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.Dominio/Usuario.cs
@@ -8,26 +8,25 @@ namespace LojaNinja.Dominio
 {
     public class Usuario
     {
-        public Usuario(string primeiroNome, string ultimoNome, string cpf, DateTime dataNascimento, string email, string senha)
+        public Usuario(string nome, string email, string senha, string permissao)
         {
-            PrimeiroNome = primeiroNome;
-            UltimoNome = ultimoNome;
-            CPF = cpf;
-            DataNascimento = dataNascimento;
+            Nome = nome;
             Email = email;
             Senha = senha;
-            DataRegistro = DateTime.Now;
-            Permissoes = new string[] {"Manager"};
+            Permissoes = new string[] { permissao };
+        }
+
+        public Usuario(string nome, string email, string senha)
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
         }
 
         public int ID { get; private set; }
-        public string PrimeiroNome { get; private set; }
-        public string UltimoNome { get; private set; }
-        public string CPF { get; private set; }
-        public DateTime DataNascimento { get; private set; }
+        public string Nome { get; private set; }
         public string Email { get; private set; }
         public string Senha { get; private set; }
-        public DateTime DataRegistro { get; private set; }
         public string[] Permissoes { get; private set; }
 
         public void AdicionarId(int id)
@@ -37,11 +36,6 @@ namespace LojaNinja.Dominio
                 throw new InvalidOperationException("Esse objeto já possuia Id, portanto ele já havia sido salvo no banco. Não é possível alterar esse valor.");
             }
             this.ID = id;
-        }
-
-        public void DarPermissao(string perm)
-        {
-            Permissoes[Permissoes.Length] = perm;
         }
     }
 }
