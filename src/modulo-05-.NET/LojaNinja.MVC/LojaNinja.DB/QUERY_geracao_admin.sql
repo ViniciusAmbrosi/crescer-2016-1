fbf0b86989ea8c15d6e7133870beebff1da69faf
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-05-16 19:19:56 -0300

    feat(QUERY_geracao_admin): adicionar query de geracao do usuario admin.

---
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.DB/QUERY_geracao_admin.sql
@@ -0,0 +1,8 @@
+INSERT INTO Usuario (nome, email, senha)
+	VALUES('Administrador', 'admin@admin.com', '63874adc5789a6e2e1fc51e40871dd53')
+
+INSERT INTO PermissaoUsuario(usuarioId, permissaoId)
+	VALUES(1, 1)
+
+INSERT INTO PermissaoUsuario(usuarioId, permissaoId)
+	VALUES(1, 2)    
