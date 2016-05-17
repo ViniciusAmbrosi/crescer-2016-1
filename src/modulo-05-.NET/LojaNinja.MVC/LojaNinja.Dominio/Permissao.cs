08d9c85a8b2a8507e549ed55f44a2a19de70c6b5
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-05-17 18:30:56 -0300

    feat(Permissao): adicionar classe de permissao.

---
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.Dominio/Permissao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNinja.Dominio
+{
+    public class Permissao
+    {
+        private Permissao()
+        {
+
+        }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public IList<Usuario> Usuarios { get; set; }
+    }
+}
