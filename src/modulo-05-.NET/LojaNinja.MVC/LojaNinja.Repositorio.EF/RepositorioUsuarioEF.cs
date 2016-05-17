7bed82ccb618b9bb5329bf28b5fdefb5249d5411
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-05-17 18:28:57 -0300

    feat(RepositorioUsuarioEF): adicionar repositorio de usuarios usando EF.

---
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.Repositorio.EF/RepositorioUsuarioEF.cs
@@ -0,0 +1,27 @@
+﻿using LojaNinja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNinja.Repositorio.EF
+{
+    public class RepositorioUsuarioEF : IUsuarioRepositorio
+    {
+        public Usuario BuscarUsuarioPorAutenticacao(string email, string senha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InserirUsuario(Usuario user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> ObterListaDeUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
