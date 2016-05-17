206b58c587044cadc9afa61358b1530ad6a5f241
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-05-17 18:29:44 -0300

    refactor(Usuario): adicionar lista para gerar tabela por entity.

--- src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.Dominio/Usuario.cs
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.Dominio/Usuario.cs
@@ -8,11 +8,16 @@ namespace LojaNinja.Dominio
 {
     public class Usuario
     {
+        public Usuario()
+        {
+
+        }
         public int ID { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
-        public string[] Permissoes { get; set; }
+        public IList<Permissao> Permissoes { get; set; }
+        public string[] permRepDB { get; set; }
 
         public void AdicionarId(int id)
         {
