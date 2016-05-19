905e908b8fd5a75d5ba7c2338a5818773d06bd37
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-05-19 17:19:03 -0300

    feat(HttpCookieExtension): adicionar classe de métodos de extensão para cookies.

---
+++ src/modulo-05-.NET/LojaNinjaRefactor.MVC/LojaNinjaRefactor.MVC/Extensios/HttpCookieExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinjaRefactor.MVC.Extensios
+{
+    public static class HttpCookieExtension
+    {
+        public static void Expirar(this HttpCookie cookie)
+        {
+            cookie.Expires = DateTime.Now.AddDays(-1d);
+        }
+    }
+}
