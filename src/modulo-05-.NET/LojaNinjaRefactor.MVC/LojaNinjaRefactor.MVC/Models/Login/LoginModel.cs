8dc0e90d0b9e97138e0bb0cd71f617aa6c925c13
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-05-19 17:17:43 -0300

    feat(LoginModel): adicionar classe da model usada para login.

---
+++ src/modulo-05-.NET/LojaNinjaRefactor.MVC/LojaNinjaRefactor.MVC/Models/Login/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinjaRefactor.MVC.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Senha")]
+        public string Senha { get; set; }
+    }
+}
