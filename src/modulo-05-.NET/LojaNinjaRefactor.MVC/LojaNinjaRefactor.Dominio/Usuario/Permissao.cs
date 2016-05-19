36ca9cef08f85922794d30030b5e8f595769a411
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-05-19 17:23:02 -0300

    feat(Permissao): adicionar classe permissao de dominio.

---
+++ src/modulo-05-.NET/LojaNinjaRefactor.MVC/LojaNinjaRefactor.Dominio/Usuario/Permissao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNinjaRefactor.Dominio.Usuario
+{
+    public class Permissao
+    {
+        public Permissao()
+        {
+        }
+
+        public Permissao(int id, string nome)
+        {
+            this.ID = id;
+            this.Nome = nome;
+        }
+
+        public int ID { get; private set; }
+        public string Nome { get; private set; }
+        public IList<Usuario> Usuarios { get; private set; }
+    }
+}
