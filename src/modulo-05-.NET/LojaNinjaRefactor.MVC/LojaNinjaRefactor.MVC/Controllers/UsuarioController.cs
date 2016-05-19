4f9f8ecc608098296fed1fa33060f487ba44c74b
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-05-19 17:19:43 -0300

    feat(UsuarioController): adicionar classe controller de actions de usuario.

---
+++ src/modulo-05-.NET/LojaNinjaRefactor.MVC/LojaNinjaRefactor.MVC/Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+﻿using LojaNinjaRefactor.MVC.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaNinjaRefactor.MVC.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        //TODO: implementar tratamento dos métodos
+
+        public ActionResult CadastroDeUsuario()
+        {
+            return View();
+        }
+
+        [Autorizacao(Roles = "COMUM")]
+        public ActionResult DetalhesUsuario()
+        {
+            return View();
+        }
+
+        [Autorizacao(Roles = "ADMIN")]
+        public ActionResult Listagemusuarios()
+        {
+            return View();
+        }
+    }
+}
