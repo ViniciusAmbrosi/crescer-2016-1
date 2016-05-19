dbe5fbabbeb1cbd825e01ef7611c0e9612a334b5
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-05-19 17:17:18 -0300

    feat(UsuarioLogadoModel): adicionar classe da model do usuario logado.

---
+++ src/modulo-05-.NET/LojaNinjaRefactor.MVC/LojaNinjaRefactor.MVC/Models/Usuario/UsuarioLogadoModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinjaRefactor.MVC.Models
+{
+    public class UsuarioLogadoModel
+    {
+        //TODO: implementar construtor baseado na class Usuario immplementada para o EF
+        //public UsuarioLogadoModel(Usuario usuario)
+        //{
+        //    Nome = usuario.Nome;
+        //    Email = usuario.Email;
+        //    Permissoes = usuario.permRepDB;
+        //    ID = usuario.ID;
+        //}
+
+        public int ID { get; private set; }
+        public string Email { get; private set; }
+        public string Nome { get; private set; }
+        public IList<string> Permissoes { get; private set; }
+
+        public bool TemPermissao(string permissao)
+        {
+            return Permissoes != null
+                   && Permissoes.Any(p => p.Equals(permissao));
+        }
+    }
+}
