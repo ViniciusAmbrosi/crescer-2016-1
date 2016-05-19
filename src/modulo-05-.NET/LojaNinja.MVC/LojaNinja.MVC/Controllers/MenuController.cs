d51b2b9ffa1ad089224f2f7e92eb1adcc3f18ed7
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-05-19 17:22:33 -0300

    feat(MenuController): adicionar classe controller da partial view menu.

---
+++ src/modulo-05-.NET/LojaNinja.MVC/LojaNinja.MVC/Controllers/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaNinja.MVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        public PartialViewResult Menu ()
+        {
+            return PartialView("Menu");
+        }
+    }
+}
