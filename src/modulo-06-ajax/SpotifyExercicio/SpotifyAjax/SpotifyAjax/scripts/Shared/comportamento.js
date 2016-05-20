11ede8c55e3a59978366dfbe727dc7c865a77c10
Vinicius A <Vinicius A>
2016-05-20 09:37:14 -0300

    feat(comportamento): adicionar script com algoritmo de inserir imagens na tela.

---
+++ src/modulo-06-ajax/SpotifyExercicio/SpotifyAjax/SpotifyAjax/scripts/Shared/comportamento.js
@@ -0,0 +1,20 @@
+﻿function exibirAlbums(caminho) {
+    $.ajax({ url: caminho, type: "GET" })
+        .done(function (res) {
+            res.items.forEach(function (item) {
+                var url = item.images[1].url;
+                var imgAlbum = new Image();
+                imgAlbum.src = url;
+                imgAlbum.onload = function () {
+                    var $img = $(imgAlbum);
+                    $("#album-cover").append($('<div>').addClass("col-lg-3").addClass("col-md-4").addClass("col-xs-6").append($img));
+                };
+            });
+        })
+        .fail(function (res) {
+            console.error(res);
+            var criarSpanComErro = function (msg) { return $('<span>').text(msg).addClass('erro'); };
+            $('#album-cover')
+            .append(criarSpanComErro("Caro usuário, devido a um erro <" + res.status + ">, não foi possível pesquisar por <" + id + ">."))
+        });
+};
