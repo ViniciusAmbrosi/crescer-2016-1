7c99a0bc67428216da3ea9f87ebbb52ca837074b
Vinicius A <Vinicius A>
2016-05-19 23:02:45 -0300

    feat(donzela): adicionar js de comportamento da pagina donzela-de-ferro.

---
+++ src/modulo-06-ajax/SpotifyExercicio/SpotifyAjax/SpotifyAjax/scripts/Custom/donzela.js
@@ -0,0 +1,12 @@
+﻿$.ajax({ url: "https://api.spotify.com/v1/artists/6mdiAmATAx73kdxrNrnlao/albums?limit=50", type: 'GET' })
+    .done(function (res) {
+        res.items.forEach(function (item) {
+            var url = item.images[1].url;
+            var imgAlbum = new Image();
+            imgAlbum.src = url;
+            imgAlbum.onload = function () {
+                var $img = $(imgAlbum);
+                $("#album-cover").append($('<div>').addClass("col-lg-3").addClass("col-md-6").addClass("col-xs-12").append($img));
+            }
+        });
+    });
