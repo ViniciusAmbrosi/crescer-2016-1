a38bc895a1354607050d1aa12d39a08fe0c8c486
Vinicius A <Vinicius A>
2016-05-21 16:22:39 -0300

    feat(DateExtensios): adicionar classe de extensão de suporte para datas.

---
+++ src/modulo-06-ajax/CdZ/src/CdZ.MVC/Extensions/DateExtensions.cs
@@ -0,0 +1,14 @@
+﻿ using System;
+using System.Globalization;
+
+namespace CdZ.MVC.Extensions
+{
+    public static class DateTimeExtensions
+    {
+
+        public static string FromDateTimeToISOString(this DateTime date)
+        {
+            return date.ToString("s", CultureInfo.InvariantCulture);
+        }
+    }
+} 
