52db4cd987ada3a7adcc7027b45f6cae5e672d0d
Vinicius A <Vinicius A>
2016-05-21 16:22:16 -0300

    feat(StringExtensios): adicionar classe extensão de iso datetime parse.

---
+++ src/modulo-06-ajax/CdZ/src/CdZ.MVC/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace CdZ.MVC.Extensions
+{
+    public static class StringExtensions
+    {
+        public static DateTime FromISOToDateTime(this string isoDate)
+        {
+            var date = DateTime.Parse(isoDate, null, DateTimeStyles.RoundtripKind);
+            return date;
+        }
+    }
+} 
