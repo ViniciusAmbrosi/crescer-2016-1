280d024d9e7e5c72945fd2afbb1b8e11a97ba4fe
Vinicius A <Vinicius A>
2016-05-21 16:22:53 -0300

    feat(MenuController): adicionar controller para partialview de menu.

---
+++ src/modulo-06-ajax/CdZ/src/CdZ.MVC/Controllers/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CdZ.MVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        public PartialViewResult Menu()
+        {
+            return PartialView("Menu");
+        }
+    }
+}
