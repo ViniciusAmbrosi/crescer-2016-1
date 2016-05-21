17d411f8793b31e19755a2a1f206c175f4a2691c
Vinicius A <Vinicius A>
2016-05-21 16:27:07 -0300

    refactor(CavaleiroViewModel): tratar quando não são passados golpes e/ou imagens e adicionar tratamento de data.

--- src/modulo-06-ajax/CdZ/src/CdZ.MVC/Models/Cavaleiro/CavaleiroViewModel.cs
+++ src/modulo-06-ajax/CdZ/src/CdZ.MVC/Models/Cavaleiro/CavaleiroViewModel.cs
@@ -3,6 +3,9 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using Dominio = CdZ.Dominio;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Script.Serialization;
+using CdZ.MVC.Extensions;
 
 namespace CdZ.MVC.Models.Cavaleiro
 {
@@ -12,25 +15,30 @@ namespace CdZ.MVC.Models.Cavaleiro
         public string Nome { get; set; }
         public double AlturaCm { get; set; }
         public double PesoLb { get; set; }
-        public DateTime DataNascimento { get; set; }
+        public string DataNascimento { get; set; }
+        [ScriptIgnore]
+        public DateTime DataNascimentoObj
+        {
+            get { return DataNascimento.FromISOToDateTime(); }
+            set { DataNascimento = value.FromDateTimeToISOString(); }
+        }
         public Signo Signo { get; set; }
         public TipoSanguineo TipoSanguineo { get; set; }
         public Local LocalNascimento { get; set; }
         public Local LocalTreinamento { get; set; }
         public IList<string> Golpes { get; set; }
         public IList<ImagemViewModel> Imagens { get; set; }
-        public CavaleiroViewModel()
-        {
-            // TODO: corrigir model-bind
-            DataNascimento = DateTime.Today;
-        }
 
         public Dominio.Cavaleiro ToModel()
         {
-            var golpesObj = Golpes.Select(_ => new Golpe(_)).ToList();
-            var imagensObj = Imagens.Select(_ => _.ToModel()).ToList();
+            var golpesObj = new List<Golpe>();
+            var imagensObj = new List<Imagem>();
+            if (Golpes != null)
+                golpesObj = Golpes.Select(_ => new Golpe(_)).ToList();
+            if (Imagens != null)
+                imagensObj = Imagens.Select(_ => _.ToModel()).ToList();
 
-            return new Dominio.Cavaleiro(Nome, AlturaCm, PesoLb, DataNascimento, Signo, TipoSanguineo, LocalNascimento, LocalTreinamento, golpesObj, imagensObj);
+            return new Dominio.Cavaleiro(Nome, AlturaCm, PesoLb, DataNascimentoObj, Signo, TipoSanguineo, LocalNascimento, LocalTreinamento, golpesObj, imagensObj);
         }
     }
 }
