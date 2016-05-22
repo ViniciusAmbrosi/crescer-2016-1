00e55ebb6f1b589375fec9e8171c90fec2aaa7c3
Vinicius A <Vinicius A>
2016-05-22 12:01:17 -0300

    feat(html-helper): adicionar função de appendar cavaleiros com botoes na tela.

---
+++ src/modulo-06-ajax/CdZ/src/CdZ.MVC/Scripts/Views/Cavaleiro/html-helper.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+var htmlHelper = {}
+
+htmlHelper.adicionarCavaleiroComBotoes = function (cava) {
+    var $cavaleiros = $('#cavaleiros');
+    var $liCavaleiro = $('<li>').append(cava.Nome);
+    $cavaleiros.append($liCavaleiro);
+    idUltimoCavaleiro = cava.Id;
+    var $btnExcluir = $("<button>").attr('data-id-btn', cava.Id).addClass('btn btn-danger');
+    $liCavaleiro.append($btnExcluir);
+    $btnExcluir.click(excluirCavaleiro);
+    $btnExcluir.text("Excluir");
+};
