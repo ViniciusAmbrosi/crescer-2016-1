17ea2115725810825e0b3b1549afe6c1aa57788c
Vinicius A <Vinicius A>
2016-05-23 12:15:42 -0300

    feat(html-helper): adicionar métodos de inclusão no form ao clicar em botao addimagem e addgolpe.

--- src/modulo-06-ajax/CdZ/src/CdZ.MVC/Scripts/Views/Cavaleiro/html-helper.js
+++ src/modulo-06-ajax/CdZ/src/CdZ.MVC/Scripts/Views/Cavaleiro/html-helper.js
@@ -7,8 +7,44 @@ htmlHelper.adicionarCavaleiroComBotoes = function (cava) {
     var $liCavaleiro = $('<li>').append(cava.Nome);
     $cavaleiros.append($liCavaleiro);
     idUltimoCavaleiro = cava.Id;
-    var $btnExcluir = $("<button>").attr('data-id-btn', cava.Id).addClass('btn btn-danger');
+
+    var $btnExcluir = $("<button>").attr('data-id-btn', cava.Id).addClass('btn btn-danger').addClass("btnExcluir");
     $liCavaleiro.append($btnExcluir);
     $btnExcluir.click(excluirCavaleiro);
     $btnExcluir.text("Excluir");
+
+    var $btnEditar = $("<button>").attr('data-id-btn', cava.Id).addClass('btn btn-danger').addClass("btnEditar");
+    $liCavaleiro.append($btnEditar);
+    $btnEditar.click(editarCavaleiro);
+    $btnEditar.text("Editar");
 };
+
+htmlHelper.gerarInputUrl = function () {
+    var $novoInput = $('<input>').attr('name', 'Imagens.imagem.Url')
+                               .attr('type', 'text')
+                               .attr("id", "Imagens_imagem_Url")
+                               .attr('placeholder', 'Ex: bit.ly/shiryu.png')
+                               .addClass("form-control").addClass("text-box").addClass("single-line");
+    return $('<li>').append($novoInput);
+}
+
+htmlHelper.gerarInputThumb = function () {
+    var $novoInput = $('<input>').attr('name', 'Imagens.imagem.IsThumb')
+                                 .attr('type', 'checkbox')
+                                 .attr("id", "Imagens.imagem.IsThumb")
+                                 .attr("checked", "checked")
+                                 .attr("data-val", "true")
+                                 .attr("data-val-required", "The IsThumb field is required.")
+                                 .attr("value", "true");
+    return $('<li>').append($novoInput);
+}
+
+htmlHelper.gerarInputTexto = function () {
+    var $novoInput = $('<input>').attr('name', 'Golpes.golpe')
+                                 .attr('type', 'text')
+                                 .attr("id", "Golpes_golpe")
+                                 .attr('placeholder', 'Ex: Meteoro de pegasus')
+                                 .addClass("form-control").addClass("text-box").addClass("single-line");
+    return $('<li>').append($novoInput);
+}
+
