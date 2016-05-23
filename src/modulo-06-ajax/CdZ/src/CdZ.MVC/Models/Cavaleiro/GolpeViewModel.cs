3443571e6389478c40b37dbcd37fefa60efed9ac
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-05-23 17:13:48 -0300

    feat(GolpeViewModel): adicionar ViewModel para golpes.

---
+++ src/modulo-06-ajax/CdZ/src/CdZ.MVC/Models/Cavaleiro/GolpeViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace CdZ.MVC.Models.Cavaleiro
+{
+    public class GolpeViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public Dominio.Golpe ToModel()
+        {
+            return new Dominio.Golpe(Id, Nome);
+        }
+
+        public GolpeViewModel toViewModel(Dominio.Golpe golpe)
+        {
+            var golpeVM = new GolpeViewModel();
+            golpeVM.Id = golpe.Id;
+            golpeVM.Nome = golpe.Nome;
+            return golpeVM;
+        }
+    }
+}
