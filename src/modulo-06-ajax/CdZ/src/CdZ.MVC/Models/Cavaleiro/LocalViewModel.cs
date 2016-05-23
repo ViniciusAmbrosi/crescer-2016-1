5a632f99ff2a6b646030f1c9ac7297e5d283b4c8
Vinicius Ambrosi <vinicius.ambrosi@cwinet.local>
2016-05-23 17:14:04 -0300

    feat(LocalViewModel): adicionar view model para local.

---
+++ src/modulo-06-ajax/CdZ/src/CdZ.MVC/Models/Cavaleiro/LocalViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace CdZ.MVC.Models.Cavaleiro
+{
+    public class LocalViewModel
+    {
+        public int Id { get; set; }
+        public string Texto { get; set; }
+
+        public Dominio.Local ToModel()
+        {
+            return new Dominio.Local(Id, Texto);
+        }
+
+        public LocalViewModel ToViewModel(Dominio.Local local)
+        {
+            var localVm = new LocalViewModel();
+            localVm.Id = local.Id;
+            localVm.Texto = local.Texto;
+            return localVm;
+        } 
+    }
+}
