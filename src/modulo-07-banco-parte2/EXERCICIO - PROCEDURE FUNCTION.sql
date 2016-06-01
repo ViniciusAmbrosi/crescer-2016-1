5abcf8d31b4161c8e6c46808e4cf6bb9935f9571
Vinicius Ambrosi <ambrosi.vinicius@gmail.com>
2016-06-01 20:15:59 -0300

    feat(Exercicio - Procedure): adicionar solução para procedures e functions.

---
+++ src/modulo-07-banco-parte2/EXERCICIO - PROCEDURE FUNCTION.sql
@@ -0,0 +1,41 @@
+--1) Crie um procedimento que receba por parâmetro o IDPedido e atualize o valor do pedido conforme seus itens.
+--Valor unitário * Quantidade
+CREATE OR REPLACE PROCEDURE ATUALIZA_PEDIDO (pIDPedido in integer) AS
+BEGIN
+  UPDATE PEDIDO ped
+  SET ped.VALORPEDIDO =(SELECT SUM(PRECOUNITARIO * QUANTIDADE)
+                        FROM PEDIDOITEM
+                        WHERE IDPEDIDO = 300)
+  WHERE ped.IDPEDIDO = pIDPEDIDO;
+END;
+
+--2) Crie uma função que receba por parâmetro o IDCliente e retorne a data do último pedido realizado pelo cliente.
+CREATE OR REPLACE FUNCTION DATA_ULTIMO_PEDIDO (pIDCliente in integer) RETURN PEDIDO.DATAPEDIDO%TYPE AS
+  vData PEDIDO.DATAPEDIDO%TYPE;
+BEGIN
+  SELECT MAX(DATAPEDIDO)
+  INTO vData
+  FROM PEDIDO
+  WHERE IDCLIENTE = pIDCliente;
+  
+  RETURN vData;
+END;
+  
+
+--3) Crie uma função que receba por parâmetro o IDProduto e o período (mês e ano) e retorne a quantidade (total)
+--desde produtos vendidos no período (considere todos os dias do mês).
+CREATE OR REPLACE FUNCTION PRODUTOS_VENDIDOS_NO_MES (pIDProduto in integer, pData in DATE) RETURN PEDIDOITEM.QUANTIDADE%TYPE AS
+  vQuantidade PEDIDOITEM.QUANTIDADE%TYPE;
+BEGIN
+  SELECT SUM(peditem.QUANTIDADE)
+  INTO vQuantidade
+  FROM PEDIDOITEM peditem
+  INNER JOIN PEDIDO ped ON peditem.IDPEDIDO = ped.IDPEDIDO
+  WHERE ped.DATAPEDIDO BETWEEN TRUNC(pData, 'MM') AND LAST_DAY(pDATA) AND
+        peditem.IDPRODUTO = pIDProduto;
+        
+  RETURN vQuantidade;
+END;
+
+
+
