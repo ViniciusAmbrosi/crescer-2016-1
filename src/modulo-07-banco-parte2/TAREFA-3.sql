ab56a4857e99ce402fd6560fc2f7b351b2af9b30
Vinicius A <Vinicius A>
2016-06-08 08:35:04 -0300

    feat(Tarefdas-DB2): adicionar resolução das tarefas de banco que nao havia comitado.

--- src/modulo-07-banco-parte2/TAREFA-3.sql
+++ src/modulo-07-banco-parte2/TAREFA-3.sql
@@ -1,16 +1,3 @@
-create or replace package PCK_CONCURSO is
-
-  -- Author  : ANDRENUNES
-  -- Created : 03/06/2016 10:40:29
-  -- Purpose : Gerar informações reference a cada concurso
-  -- Public type declarations
-  
-  procedure geraProximoConcurso;
-  procedure atualizaAcertadores;
-
-end PCK_CONCURSO;
-/
-
 create or replace package body PCK_CONCURSO is
 
 procedure geraProximoConcurso as
@@ -57,36 +44,89 @@ procedure atualizaAcertadores as
 
    vContadorAcertos INTEGER;   
    vAcumulou BOOLEAN := TRUE;
+   vPrimeiraAposta INTEGER;
+   vVencedoresSena INTEGER := 0;
+   vVencedoresQuina INTEGER := 0;
+   vVencedoresQuadra INTEGER := 0;
+   vValorSena DECIMAL(12,2);
+   vValorQuina DECIMAL(12,2);
+   vValorQuadra DECIMAL(12,2);
+   vConcursoAtual INTEGER;
    
-  CURSOR c_apostas IS
-      SELECT ap.IDCONCURSO, ap.IDAPOSTA,  ap.Valor
+  CURSOR c_apostas (pIDConcurso INTEGER)IS
+      SELECT ap.IDAPOSTA
       FROM APOSTA ap
       LEFT JOIN APOSTA_PREMIADA ap_p ON ap.IDAPOSTA = ap_p.IDAPOSTA
-      WHERE ap_p.IDAPOSTA IS NULL; 
+      WHERE ap_p.IDAPOSTA IS NULL AND ap.IDCONCURSO = pIDConcurso
+      ORDER BY ap.IDAPOSTA ASC; 
+      
+  CURSOR c_concursos IS
+      SELECT IDCONCURSO
+      FROM CONCURSO
+      ORDER BY IDCONCURSO;
       
   BEGIN
+    vPrimeiraAposta := 0;
     vContadorAcertos := 0;
-    
-    FOR aposta IN c_apostas LOOP
-       vContadorAcertos := NumerosAposta(aposta.IDAPOSTA, aposta.IDCONCURSO);
+    SYS.DBMS_OUTPUT.PUT_LINE(vConcursoAtual);
+    FOR concurso in c_concursos LOOP
+      vConcursoAtual := concurso.IDCONCURSO;
+      SELECT PREMIO_SENA, PREMIO_QUINA, PREMIO_QUADRA
+      INTO  vValorSena, vValorQuina, vValorQuadra
+      FROM CONCURSO
+      WHERE IDCONCURSO = vConcursoAtual;
+      
+      FOR aposta IN c_apostas(concurso.IDCONCURSO) LOOP
+       vContadorAcertos := NumerosAposta(aposta.IDAPOSTA, vConcursoAtual);
        
        IF(vContadorAcertos > 3) THEN
          INSERT INTO APOSTA_PREMIADA (IDAPOSTA, ACERTOS, VALOR)
-         VALUES(aposta.IDAPOSTA, vContadorAcertos, aposta.Valor);
+         VALUES(aposta.IDAPOSTA, vContadorAcertos, 0);   
+         IF(vContadorAcertos = 4)THEN
+          vVencedoresQuadra := vVencedoresQuadra + 1;
+         ELSIF(vContadorAcertos = 5) THEN
+          vVencedoresQuina := vVencedoresQuina + 1;
+         ELSIF (vContadorAcertos = 6) THEN
+          vVencedoresSena := vVencedoresSena + 1;
+         END IF;
        END IF;
-       
        IF(vContadorAcertos = 6 AND vAcumulou) THEN
           vAcumulou := false;
           UPDATE CONCURSO
           SET ACUMULOU = 1
-          WHERE IDCONCURSO = aposta.IDCONCURSO;
+          WHERE IDCONCURSO = vConcursoAtual;
        END IF;
+      END LOOP;
+      
+      IF(vVencedoresSena != 0) THEN
+        UPDATE APOSTA_PREMIADA
+        SET VALOR = vValorSena / vVencedoresSena
+        WHERE ACERTOS = 6 AND IDAPOSTA_PREMIADA > vPrimeiraAposta;
+      END IF;
+      
+      IF(vVencedoresQuina != 0) THEN
+        UPDATE APOSTA_PREMIADA
+        SET VALOR = vValorQuina / vVencedoresQuina
+        WHERE ACERTOS = 5 AND IDAPOSTA_PREMIADA > vPrimeiraAposta;
+      END IF;
+       
+      IF(vVencedoresQuadra != 0) THEN
+        UPDATE APOSTA_PREMIADA
+        SET VALOR = vValorQuadra / vVencedoresQuadra
+        WHERE ACERTOS = 4 AND IDAPOSTA_PREMIADA > vPrimeiraAposta;
+      END IF;
+      
+      SELECT MAX(IDAPOSTA_PREMIADA)
+      INTO vPrimeiraAposta
+      FROM APOSTA_PREMIADA;
+      
+      vVencedoresSena := 0;
+      vVencedoresQuina := 0;
+      vVencedoresQuadra := 0;
     END LOOP;
-    
   END;
   ----------------------------------------------------------------------
   
 begin
   null;
 end PCK_CONCURSO;
-/
