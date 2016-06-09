74b271b6d42a4daf64d69d4ae2f27a381b994a79
Vinicius A <Vinicius A>
2016-06-09 10:55:34 -0300

    test(MeuWriterUtilsTest): adicionar testes para meu writer.

---
+++ src/modulo-08-JAVA/Tarefa2/src/test/java/br/com/cwi/tarefa2/MeuWriterUtilsTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cwi.tarefa2;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dream
+ */
+public class MeuWriterUtilsTest {
+    
+    @Test
+    public void testEscreverNoArquivo() throws Exception {
+        System.out.println("escreverNoArquivo");
+        String nome = "criar.txt";
+        List<String> conteudo = new ArrayList<String>();
+        conteudo.add("Ta funcionando?");
+        String expResult = "Ta funcionando?/n";
+        String result =  MeuWriterUtils.escreverNoArquivo(nome, conteudo);
+        assertEquals(expResult, result);
+    }
+    
+}
