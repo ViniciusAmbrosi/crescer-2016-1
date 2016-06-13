b7259db06ea45a5a7e1097a160890e9ba8c7de37
Vinicius A <Vinicius A>
2016-06-13 11:02:53 -0300

    feat(MaterialCSV): adicionar metodo para gerar CSV.

--- src/modulo-08-JAVA/Tarefa4/src/main/java/br/com/tarefa4/DAO/MaterialDAO.java
+++ src/modulo-08-JAVA/Tarefa4/src/main/java/br/com/tarefa4/DAO/MaterialDAO.java
@@ -1,8 +1,11 @@
 package br.com.tarefa4.DAO;
 
 import br.com.cwi.tarefa4.interfaces.IMaterial;
+import br.com.cwi.tarefa4.utils.CSVUtils;
 import br.com.tarefa4.entity.Cidade;
+import br.com.tarefa4.entity.Cliente;
 import br.com.tarefa4.entity.Material;
+import java.io.IOException;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -79,4 +82,8 @@ public class MaterialDAO implements IMaterial {
         return material;
     }
 
+    public void exportarCsv(String caminho) throws IOException {
+        List<Material> materiais = listAll();
+        CSVUtils.gerarCsvMaterial(materiais, caminho);
+    }
 }
