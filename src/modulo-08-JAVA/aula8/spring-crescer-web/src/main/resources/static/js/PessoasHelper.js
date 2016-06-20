d5b2e891b1b2a05acad5c76d3469c739313a4359
Vinicius A <Vinicius A>
2016-06-20 10:56:05 -0300

    feat(alterar-pessoa): adicionar function para alterar pessoa e atualizar pvs ao manipular pessoas.

--- src/modulo-08-JAVA/aula8/spring-crescer-web/src/main/resources/static/js/PessoasHelper.js
+++ src/modulo-08-JAVA/aula8/spring-crescer-web/src/main/resources/static/js/PessoasHelper.js
@@ -6,5 +6,18 @@ PessoasHelper.deletarPessoa = function (id) {
     return $.ajax({
         url: "/Pessoa?id=" + id,
         type: 'DELETE'
+    }).done(function(res) {
+        $('#tabela-pessoa').empty();
+        $('#tabela-pessoa').append(res);
+    });;
+};
+
+PessoasHelper.alterarPessoa = function (id) {
+    return $.ajax({
+        url: "Pessoa/Update?id=" + id,
+        type:'GET'
+    }).done(function(res) {
+        $('#form-pessoa').empty();
+        $('#form-pessoa').append(res);
     });
 };
