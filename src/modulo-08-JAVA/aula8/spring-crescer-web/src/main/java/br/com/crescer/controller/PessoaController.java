53743d65f7021745b08a15e120b095d37d9c799a
Vinicius A <Vinicius A>
2016-06-20 10:57:09 -0300

    feat(update): adicionar método para update e alterar métodos existentes de forma a utilizarem pvs corretamente.

--- src/modulo-08-JAVA/aula8/spring-crescer-web/src/main/java/br/com/crescer/controller/PessoaController.java
+++ src/modulo-08-JAVA/aula8/spring-crescer-web/src/main/java/br/com/crescer/controller/PessoaController.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,21 +26,38 @@ public class PessoaController {
     PessoaService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public String list(Model model) {
-        model.addAttribute("pessoa", new Pessoa());
-        model.addAttribute("pessoas", service.findAll());
+    public String index(Pessoa p, Model model) {
+        form(p, model);
+        list(model);
         return "index";
+    }
+
+    public String form(Pessoa p, Model model){
+        model.addAttribute("pessoa", p == null ? new Pessoa() : p);
+        return "form-pessoa";
+    }
+    
+    public String list(Model model) {
+        model.addAttribute("pessoas", service.findAll());
+        return "tabela-pessoa";
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public String save(@ModelAttribute Pessoa p, Model model) {
-        p.setData(new Date());
+        if(p.getData() == null) p.setData(new Date());
         service.save(p);
-        return list(model); //atualizar lista
+        return index(null, model); //atualizar lista
     }
 
     @RequestMapping(method = RequestMethod.DELETE)
-    public void delete(String id) {
+    public String delete(String id, Model model) {
         service.delete(Long.parseLong(id));
+        return list(model); //atualizar lista
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/Update")
+    public String update(String id, Model model) {
+        Pessoa p = service.findById(Long.parseLong(id));
+        return form(p, model);
     }
 }
